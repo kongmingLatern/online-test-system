1572a2eba04f3f0563d3f04955ea1f19ceab4712
凤之兮原 <1577310596@qq.com>
2022-11-17 23:46:30 +0800

    feat: add antd-vue and unocss

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
 
-import './assets/main.css'
+import 'ant-design-vue/dist/antd.css';
+import 'uno.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
