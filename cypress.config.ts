17a9b547a6642a17582b8d50ffb7bfa141c732d8
凤之兮原 <1577310596@qq.com>
2022-11-21 16:39:49 +0800

    chore: change cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
-  }
+  },
 })
--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,19 @@
 // https://docs.cypress.io/api/introduction/api.html
 
+
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'You did it!')
   })
 })
+
+
+
+// describe('Header', () => {
+//   it('we should have a Header component', () => {
+//     const wrapper = mount(StudentInfoUpdateVue)
+//     console.log(wrapper);
+//   })
+// });
+
