6a79a12e97085491c8652b2567b167a7c4413f10
凤之兮原 <1577310596@qq.com>
2022-11-21 21:24:11 +0800

    feat: add function findTag

--- src/utils/vueUtils.ts
+++ src/utils/vueUtils.ts
@@ -5,3 +5,6 @@ import type { DefineComponent } from 'vue';
 export function findComponent(componentName: DefineComponent<any, any, any, any>, element: string): DOMWrapper<Element> {
   return mount(componentName).find(`[data-test=${element}]`)
 }
+export function findTag(componentName: DefineComponent<any, any, any, any>, tag: string): DOMWrapper<Element> {
+  return mount(componentName).find(`${tag}`)
+}
