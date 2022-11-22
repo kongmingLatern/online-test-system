700aca60fb3843a6638d4befea62c245b3b4130f
凤之兮原 <1577310596@qq.com>
2022-11-22 23:58:29 +0800

    rename

---
+++ src/views/admin/Content.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 头部 -->
+  <header data-test="header">
+    <slot name="header"></slot>
+  </header>
+  <!-- 导航栏 -->
+  <nav data-test="nav">
+    <slot name="nav"></slot>
+  </nav>
+  <!-- 主要内容 -->
+  <main data-test="main">
+    <slot name="main"></slot>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
