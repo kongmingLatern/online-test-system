dddb689ba1adeb8fe375e5bf523cb2cc982b195f
凤之兮原 <1577310596@qq.com>
2022-11-22 22:06:32 +0800

    feat: add new Component

--- src/mock/ant-design-vue.ts
+++ src/mock/ant-design-vue.ts
@@ -3,7 +3,12 @@ const mockComponent = {
   template: '<div><slot></slot></div>',
 };
 
-export const globalComponents = {
+const AntdIcon = {
+  'video-camera-outlined': mockComponent,
+  'user-outlined': mockComponent,
+}
+
+const Antd = {
   'a-layout': mockComponent,
   'a-layout-sider': mockComponent,
   'a-layout-header': mockComponent,
@@ -12,5 +17,42 @@ export const globalComponents = {
   'a-button': mockComponent,
   'a-menu': mockComponent,
   'a-menu-item': mockComponent,
+  'a-menu-item-group': mockComponent,
+  'a-sub-menu': mockComponent,
+  'a-input': mockComponent,
+  'a-form': mockComponent,
+  'a-form-item': mockComponent,
+  'a-select': mockComponent,
+  'a-option': mockComponent,
+  'a-upload': mockComponent,
+  'a-table': mockComponent,
+  'a-table-column': mockComponent,
+  'a-pagination': mockComponent,
+  'a-popconfirm': mockComponent,
+  'a-space': mockComponent,
+  'a-tag': mockComponent,
+  'a-divider': mockComponent,
+  'a-input-number': mockComponent,
+  'a-radio': mockComponent,
+  'a-radio-group': mockComponent,
+  'a-radio-button': mockComponent,
+  'a-checkbox': mockComponent,
+  'a-checkbox-group': mockComponent,
+  'a-switch': mockComponent,
+}
+
+const vueComponent = {
   'router-link': mockComponent,
+  'router-view': mockComponent,
+}
+
+const customComponent = {
+  'AdminAside': mockComponent
+}
+
+export const globalComponents = {
+  ...Antd,
+  ...AntdIcon,
+  ...customComponent,
+  ...vueComponent
 };
