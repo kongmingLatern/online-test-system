04979c9c1e30cdf63eb88fb2fd3c789d5795a83f
凤之兮原 <1577310596@qq.com>
2022-11-22 18:19:32 +0800

    feat: package some vue-test-utils

--- src/utils/vueUtils.ts
+++ src/utils/vueUtils.ts
@@ -1,10 +1,9 @@
 import type { DOMWrapper } from '@vue/test-utils';
-import { mount } from "@vue/test-utils";
-import type { DefineComponent } from 'vue';
+import type { VueWrapper } from '@vue/test-utils';
 
-export function findComponent(componentName: DefineComponent<any, any, any, any>, element: string): DOMWrapper<Element> {
-  return mount(componentName).find(`[data-test=${element}]`)
+export function findComponent(wrapper: VueWrapper, element: string): DOMWrapper<Element> {
+  return wrapper.find(`[data-test=${element}]`)
 }
-export function findTag(componentName: DefineComponent<any, any, any, any>, tag: string): DOMWrapper<Element> {
-  return mount(componentName).find(`${tag}`)
+export function findTag(wrapper: VueWrapper, tag: string): DOMWrapper<Element> {
+  return wrapper.find(`${tag}`)
 }
