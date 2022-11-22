7ac894891b777661c676dc84c54540cca97a777b
凤之兮原 <1577310596@qq.com>
2022-11-22 18:19:08 +0800

    extract: some mock globalComponents

---
+++ src/mock/ant-design-vue.ts
@@ -0,0 +1,16 @@
+
+const mockComponent = {
+  template: '<div><slot></slot></div>',
+};
+
+export const globalComponents = {
+  'a-layout': mockComponent,
+  'a-layout-sider': mockComponent,
+  'a-layout-header': mockComponent,
+  'a-layout-content': mockComponent,
+  'a-layout-footer': mockComponent,
+  'a-button': mockComponent,
+  'a-menu': mockComponent,
+  'a-menu-item': mockComponent,
+  'router-link': mockComponent,
+};
