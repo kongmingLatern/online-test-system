354a109365e18997892ff157fbb44f2e8d726a63
凤之兮原 <1577310596@qq.com>
2022-11-22 22:30:50 +0800

    feat: add AdminAside test cases

--- src/__tests__/components/AdminAside.spec.ts
+++ src/__tests__/components/AdminAside.spec.ts
@@ -8,11 +8,22 @@ beforeEach(() => {
   wrapper = RegistGlobalComponent(AdminAside)
 })
 
-describe('AdminAside', () => {
+describe('AdminAside tab', () => {
   it('shoule have a tab', () => {
     const tab = findComponent(wrapper, "tab")
     expect(tab.exists()).toBe(true)
   });
+  it('tab shoule have 学生信息上传', () => {
+    const tab = findComponent(wrapper, "tab")
+    expect(tab.text()).toBe('学生信息上传');
+  });
+  it('tab shoule have 学生信息上传', () => {
+    const tab1 = findComponent(wrapper, "tab1")
+    expect(tab1.text()).toBe('学生信息管理');
+  });
+});
+
+describe('sub', () => {
   it('shoule have a sub', () => {
     const sub = findComponent(wrapper, "sub")
     expect(sub.exists()).toBe(true)
