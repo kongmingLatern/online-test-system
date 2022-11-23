e916cfc7ed367d7656d029745aa373b5a0cfdf1c
凤之兮原 <1577310596@qq.com>
2022-11-23 08:30:04 +0800

    feat: add some test cases

--- src/__tests__/pages/StudentInfoUpload.spec.ts
+++ src/__tests__/pages/StudentInfoUpload.spec.ts
@@ -14,13 +14,32 @@ let wrapper
 beforeEach(() => {
   wrapper = RegistGlobalComponent(StudentInfoUpload, options)
 })
-describe('Header', () => {
+describe('component shoule be exists', () => {
   it('we should have a Header component', () => {
     const header = findComponent(wrapper, "studentHeader")
     expect(header.exists()).toBe(true)
   })
-  it('we should have a Aside component', () => {
+  it('we should have a Content component', () => {
     const studentContent = findComponent(wrapper, "studentContent")
     expect(studentContent.exists()).toBe(true)
   })
 })
+
+describe('Content slot shoule be exists', () => {
+  it('we should have a Header component inner Content', () => {
+    const header = findComponent(wrapper, "studentHeader")
+    expect(header.exists()).toBe(true)
+  })
+  it('we should have a Nav component', () => {
+    const nav = findComponent(wrapper, "nav")
+    expect(nav.exists()).toBe(true)
+  })
+});
+
+
+describe('content component', () => {
+  it('shoule have a breadcrumb component', () => {
+    const breadcrumb = findComponent(wrapper, "breadcrumb")
+    expect(breadcrumb.exists()).toBe(true)
+  });
+});
