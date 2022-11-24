e71a127568fa4b44a2351aa75f1a90c04f7df3e8
凤之兮原 <1577310596@qq.com>
2022-11-24 22:18:56 +0800

    feat: add some component's d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,45 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
 import { presetAttributify, presetUno } from 'unocss'
 import { vitestConfig } from './viteConfig'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   test: {
-    ...vitestConfig
+    ...vitestConfig,
   },
-  plugins: [vue(), vueJsx(), Unocss({
-    presets: [presetUno(), presetAttributify()],
-    shortcuts: {
-      'header-title': 'color-white font-bold text-5 pl-10 '
-    }
-  })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Unocss({
+      presets: [presetUno(), presetAttributify()],
+      shortcuts: {
+        'header-title':
+          'color-white font-bold text-5 pl-10 ',
+      },
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true,
+      },
+    }),
+    Components({
+      dts: 'src/auto-import.d.ts',
+      extensions: ['vue'],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
