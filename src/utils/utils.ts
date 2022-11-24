33259c95349d2637387ce106f0a9e0282573f41e
凤之兮原 <1577310596@qq.com>
2022-11-24 10:32:34 +0800

    feat: finish test cases

---
+++ src/utils/utils.ts
@@ -0,0 +1,25 @@
+export function setColumn(title: string, name: string, key: string, options?: Record<string, any>): Record<string, any> {
+  return {
+    title,
+    dataIndex: name,
+    key,
+    ...options
+  }
+};
+
+export function setColumns(arr: Array<any>, options?: Record<string, any>): Record<string, any> {
+  let result: Array<Record<string, any>> = []
+  arr.forEach((item: Record<string, any>) => {
+    result.push(item)
+  })
+  return result
+};
+
+export function setData(arr: any[]): any[] {
+  return arr.map((item, index) => {
+    return {
+      key: index,
+      ...item
+    }
+  })
+}
