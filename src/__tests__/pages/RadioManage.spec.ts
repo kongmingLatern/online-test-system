dbe2a47fcfd7c080c3d81ca08207a1f592bedef6
凤之兮原 <1577310596@qq.com>
2022-11-24 16:22:00 +0800

    format

--- src/__tests__/pages/RadioManage.spec.ts
+++ src/__tests__/pages/RadioManage.spec.ts
@@ -1,14 +1,16 @@
-import Header from "@/components/Header.vue";
-import Nav from "@/components/Nav.vue";
-import { findComponent, registGlobalComponent } from "@/utils";
-import RadioManage from "@/pages/admin/RadioManage.vue";
+import Header from '@/components/Header.vue'
+import {
+  findComponent,
+  registGlobalComponent,
+} from '@/utils'
+import RadioManage from '@/pages/admin/RadioManage.vue'
 
 vitest.mock('ant-design-vue')
 
 const options = {
   props: {
-    text: ''
-  }
+    text: '',
+  },
 }
 
 let wrapper
@@ -18,39 +20,41 @@ beforeEach(() => {
 })
 describe('component shoule be exists', () => {
   it('we should have a Header component', () => {
-    const header = findComponent(wrapper, "radioHeader")
+    const header = findComponent(wrapper, 'radioHeader')
     expect(header.exists()).toBe(true)
   })
   it('we should have a Nav component', () => {
-    const radioNav = findComponent(wrapper, "radioNav")
+    const radioNav = findComponent(wrapper, 'radioNav')
     expect(radioNav.exists()).toBe(true)
   })
   it('we should have a Content component', () => {
-    const radioContent = findComponent(wrapper, "radioContent")
+    const radioContent = findComponent(
+      wrapper,
+      'radioContent'
+    )
     expect(radioContent.exists()).toBe(true)
   })
   it('we should have a Content component', () => {
-    const radioMain = findComponent(wrapper, "radioMain")
+    const radioMain = findComponent(wrapper, 'radioMain')
     expect(radioMain.exists()).toBe(true)
   })
 })
 
-
 describe('content component', () => {
   it('shoule have a breadcrumb component', () => {
-    const breadcrumb = findComponent(wrapper, "breadcrumb")
+    const breadcrumb = findComponent(wrapper, 'breadcrumb')
     expect(breadcrumb.exists()).toBe(true)
-  });
-});
+  })
+})
 
 describe('compoent props text', () => {
   it('header text', () => {
     const options = {
       props: {
-        text: '学生信息管理'
-      }
+        text: '学生信息管理',
+      },
     }
     const header = registGlobalComponent(Header, options)
     expect(header.find('h1').text()).toEqual('学生信息管理')
-  });
-});
+  })
+})
