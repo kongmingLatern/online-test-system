83865cc98b1c56c6dfe0925869009d071d6bd054
凤之兮原 <1577310596@qq.com>
2022-11-24 18:53:21 +0800

    extract RadioTableData

--- src/pages/admin/RadioManage.vue
+++ src/pages/admin/RadioManage.vue
@@ -39,60 +39,10 @@ import Nav from '@/components/Nav.vue'
 import Main from '@/components/Main.vue'
 import BreadCrumb from '@/components/BreadCrumb.vue'
 import { provide } from 'vue'
-import type { TableColumnsOptions } from '@/type'
-import { Radio, setData } from '@/utils'
-import Column from '@/utils/Task/Column'
-const options: Partial<TableColumnsOptions> = {
-  align: 'center',
-}
-const columns = [
-  new Column('题目编号', 'no', 'no', options),
-  new Column(
-    '题目',
-    'questionList',
-    'questionList',
-    options
-  ),
-  new Column(
-    '选项',
-    'questionAnswer',
-    'questionAnswer',
-    options
-  ),
-  new Column(
-    '答案',
-    'questionCorrect',
-    'questionCorrect',
-    options
-  ),
-  new Column(
-    '所属试题名称',
-    'baseTitle',
-    'baseTitle',
-    options
-  ),
-  new Column('操作', 'delete', 'delete', {
-    width: 60,
-    ...options,
-  }),
-]
-
-const data = [
-  new Radio(
-    '马克思主义',
-    '下列选项属于马克思主义三个组成部分之一的是()',
-    ['1', '2', '3', '4'],
-    ['2']
-  ),
-  new Radio(
-    '马克思主义',
-    '下列选项属于马克思主义三个组成部分之一的是()',
-    ['1', '2', '3', '4'],
-    ['2']
-  ),
-]
-const result = setData(data)
-provide('columns', columns)
+import { RadioColumn, RadioData } from '@/utils/TableData'
+import { setData } from '@/utils'
+const result = setData(RadioData)
+provide('columns', RadioColumn)
 provide('data', result)
 </script>
 
