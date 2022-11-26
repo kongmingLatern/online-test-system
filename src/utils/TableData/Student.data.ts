a0f9e5b487318ee0e5c809574c9b4202e423da9c
凤之兮原 <1577310596@qq.com>
2022-11-26 14:14:48 +0800

    chore: package StudentColumn

---
+++ src/utils/TableData/Student.data.ts
@@ -0,0 +1,17 @@
+import type { TableColumnsOptions } from '@/type'
+import { Column } from '../Task'
+
+const options: Partial<TableColumnsOptions> = {
+  align: 'center',
+}
+
+export const columns = [
+  new Column('编号', 'no', 'no', options),
+  new Column('学号', 'studentNo', 'studentNo', options),
+  new Column('姓名', 'studentName', 'studentName', options),
+  new Column('班级', 'classNo', 'classNo', options),
+  new Column('操作', 'delete', 'delete', {
+    width: 60,
+    align: 'center',
+  }),
+]
