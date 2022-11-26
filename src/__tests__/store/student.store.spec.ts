08a08424cb22e3f858a4174f3ea855dd3a2ff6c7
凤之兮原 <1577310596@qq.com>
2022-11-26 23:34:56 +0800

    feat: add snap to studentInfo

--- src/__tests__/store/student.store.spec.ts
+++ src/__tests__/store/student.store.spec.ts
@@ -1,6 +1,5 @@
 import { setActivePinia, createPinia } from 'pinia'
 import { useStudent } from '@/stores/student.store'
-import { Student } from '@/utils'
 
 describe('Student Store', () => {
   beforeEach(() => {
@@ -10,11 +9,18 @@ describe('Student Store', () => {
   // NOTE: 测试接口 'student/pages' 的数据'
   it.skip('should return 1 record', async () => {
     const store = useStudent()
-    await expect(
-      store.getStudentsByPage(1, 1)
-    ).resolves.toEqual([
-      [new Student('092218114', '沈金晶', '0922201')],
-      19,
-    ])
+    await expect(store.getStudentsByPage(1, 1)).resolves
+      .toMatchInlineSnapshot(`
+      [
+        [
+          Student {
+            "classNo": "0922201",
+            "studentName": "沈金晶",
+            "studentNo": "092218114",
+          },
+        ],
+        19,
+      ]
+    `)
   })
 })
