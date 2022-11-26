e76c67cf68511b6b5735e6c340ae02f15f73f5f3
凤之兮原 <1577310596@qq.com>
2022-11-26 14:13:52 +0800

    feat: add getQuestionByCurrentPage

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,6 +2,7 @@ import { Student } from '@/utils'
 import type { Ref } from 'vue'
 import http from './http'
 import { useStudent } from '../stores/student.store'
+import { useQuestion } from '@/stores/question.store'
 export async function getStudentDataByCurrentPage(
   data,
   currentPage,
@@ -14,6 +15,33 @@ export async function getStudentDataByCurrentPage(
       pageSize,
       currentPage
     )
+
+    res.forEach(item => {
+      data.push(item)
+    })
+
+    totalPage.value = total
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+export async function getQuestionByCurrentPage(
+  data,
+  currentPage,
+  pageSize,
+  totalPage: Ref<number | undefined>,
+  questionType
+) {
+  try {
+    const store = useQuestion()
+    const [res, total] =
+      await store.getQuestionByCurrentPage(
+        pageSize,
+        currentPage,
+        questionType
+      )
 
     res.forEach(item => {
       data.push(item)
