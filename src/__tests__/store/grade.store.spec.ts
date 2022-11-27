0de25eb507b34274ae3291f3eef58853ebf3cbc7
凤之兮原 <1577310596@qq.com>
2022-11-27 14:02:23 +0800

    feat: add grade test cases

---
+++ src/__tests__/store/grade.store.spec.ts
@@ -0,0 +1,29 @@
+import { useGrade } from '@/stores/grade.store'
+import { setActivePinia, createPinia } from 'pinia'
+
+describe('Radio Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  // NOTE: 测试接口 'student/pages' 的数据'
+  it('should return 8 record', async () => {
+    const store = useGrade()
+    await expect(store.getGradeByCurrentPage(1, 1)).resolves
+      .toMatchInlineSnapshot(`
+      [
+        [
+          Grade {
+            "baseTitle": "123",
+            "classNo": "沈金晶",
+            "grade": 99,
+            "studentName": "0922201",
+            "studentNo": "092218114",
+            "subjectName": "高数A",
+          },
+        ],
+        54,
+      ]
+    `)
+  })
+})
