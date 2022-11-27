65b813eacd7b1abd4fa0b13fe58a57d701292911
凤之兮原 <1577310596@qq.com>
2022-11-27 19:31:49 +0800

    feat: add teacher API test cases

---
+++ src/__tests__/store/teacher.store.spec.ts
@@ -0,0 +1,25 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useTeacher } from '@/stores/teacher.store'
+
+describe('Student Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  // NOTE: 测试接口 'teacher/pages' 的数据'
+  it('should return 1 record', async () => {
+    const store = useTeacher()
+    await expect(store.getTeachersByPage(1, 1)).resolves
+      .toMatchInlineSnapshot(`
+      [
+        [
+          Teacher {
+            "teacherName": "沈金晶",
+            "teacherNo": "0922181141",
+          },
+        ],
+        20,
+      ]
+    `)
+  })
+})
