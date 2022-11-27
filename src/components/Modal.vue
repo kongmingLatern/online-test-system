2a760d605573cb764a9ec6f941f1c1be41040d28
凤之兮原 <1577310596@qq.com>
2022-11-27 21:11:53 +0800

    feat: finish Modal test cases

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-modal
+    v-model:visible="visible"
+    @ok="handleOk"
+    data-test="modal"
+  >
+    <template #title>
+      <span data-test="title">{{ title || '添加' }}</span>
+    </template>
+    <slot name="modalContent"></slot>
+  </a-modal>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+defineProps<{
+  title?: string
+}>()
+
+const visible = ref<boolean>(false)
+
+const handleOk = (e: MouseEvent) => {
+  console.log(e)
+  visible.value = false
+}
+</script>
