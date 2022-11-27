1168a5e3b5afb8b5322acf9b9eb3fe9f81e51ef0
凤之兮原 <1577310596@qq.com>
2022-11-27 19:33:29 +0800

    feat: add Teacher store

---
+++ src/stores/teacher.store.ts
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia'
+import http from '../api/http'
+
+import { Teacher } from '@/utils'
+export const useTeacher = defineStore('teachers', {
+  state: () => ({
+    teacherData: [] as Teacher[],
+  }),
+
+  actions: {
+    async getTeachersByPage(
+      pageSize,
+      currentPage,
+      teacherNo?
+    ) {
+      try {
+        const res = await http.get('teacher/page', {
+          params: {
+            page: currentPage,
+            pageSize,
+            teacherNo,
+          },
+        })
+        this.teacherData = []
+        res.data.records.forEach(record => {
+          const { teacherNo, teacherName } = record
+
+          this.teacherData.push(
+            new Teacher(teacherNo, teacherName)
+          )
+        })
+        return [this.teacherData, res.data.total]
+      } catch (error) {
+        // 让表单组件显示错误
+        return [this.teacherData, 0]
+      }
+    },
+  },
+})
