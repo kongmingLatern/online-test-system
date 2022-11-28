b7e74174b6cd5a32dcfc88bc47db3d6262ae0a62
凤之兮原 <1577310596@qq.com>
2022-11-28 21:21:05 +0800

    enhance: enhance Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -25,7 +25,13 @@
         <a>点击查看详情</a>
       </template>
       <template v-if="column.dataIndex === 'delete'">
-        <a-button type="danger" rounded>删除</a-button>
+        <a-button
+          type="danger"
+          rounded
+          @click="deleteItem(record)"
+        >
+          删除
+        </a-button>
       </template>
       <template v-if="column.dataIndex === 'deleteAndAuth'">
         <a-space>
@@ -38,7 +44,13 @@
           <a-button type="primary" rounded>
             重置密码
           </a-button>
-          <a-button type="danger" rounded>删除</a-button>
+          <a-button
+            type="danger"
+            rounded
+            @click="deleteItem(record)"
+          >
+            删除
+          </a-button>
         </a-space>
       </template>
     </template>
@@ -52,9 +64,14 @@ const data = inject('data')
 const loading = inject('loading')
 const pagination = inject('pagination')
 const change: any = inject('change') ?? null
+const removeItem: any = inject('removeItem') ?? null
 
 // TODO: 孙传爷
 const changePage = pagination => {
   change(pagination)
+}
+
+const deleteItem = record => {
+  removeItem(record)
 }
 </script>
