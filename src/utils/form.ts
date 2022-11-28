3cbde3731d4a321f043e6f952c886ed4dddad4fc
凤之兮原 <1577310596@qq.com>
2022-11-28 10:12:11 +0800

    feat: finish first test cases

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,20 +1,19 @@
 import adminConfig from '@/config/admin.config'
-export default function getFormItem() {
+export default function getFormItem(data?) {
   const result: Record<string, any> = []
-  const { form } = adminConfig
+  const { form } = data || adminConfig
   const formState = {}
   const formStateKeys = Object.keys(form)
-  console.log(formStateKeys)
   formStateKeys.forEach(key => {
     if (typeof form[key] === 'object') {
       const getKeys: any = Object.values(form[key])[0]
-      // 判断该对象上的 type 属性
       const type = getKeys.type
       const index = Object.keys(form[key])[0]
       result.push(Object.values(form[key])[0])
 
       formState[key] = {}
 
+      // 判断该对象上的 type 属性
       if (type === 'text') {
         formState[key][index] = ''
       }
