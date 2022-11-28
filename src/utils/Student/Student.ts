3303b0284112994558768bdc119a241bc7873663
凤之兮原 <1577310596@qq.com>
2022-11-28 21:20:16 +0800

    feat: add a param

--- src/utils/Student/Student.ts
+++ src/utils/Student/Student.ts
@@ -1,13 +1,16 @@
 export default class Student {
+  public studentId: string = ''
   public studentNo: string = ''
   public studentName: string = ''
   public classNo: string = ''
 
   constructor(
+    studentId: string,
     studentNo: string,
     studentName: string,
     classNo: string
   ) {
+    this.studentId = studentId
     this.studentNo = studentNo
     this.studentName = studentName
     this.classNo = classNo
