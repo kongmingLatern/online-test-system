ce04092c3b19f598fdfc767377e5f5dfac5a4d27
凤之兮原 <1577310596@qq.com>
2022-11-28 14:07:23 +0800

    feat: finish test cases about form.ts

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,29 +1,31 @@
 import adminConfig from '@/config/admin.config'
-export default function getFormItem(data?) {
+export default function getFormItem(formKey, data?) {
   const formArr: Record<string, any> = []
   const { form } = data || adminConfig
   const formState = {}
   const formStateKeys = Object.keys(form)
   formStateKeys.forEach(key => {
-    if (typeof form[key] === 'object') {
-      const getValues: any[] = Object.values(form[key])
-      const getKeys = Object.keys(form[key])
-      formState[key] = {}
+    if (key === formKey) {
+      if (typeof form[key] === 'object') {
+        const getValues: any[] = Object.values(form[key])
+        const getKeys = Object.keys(form[key])
+        formState[key] = {}
 
-      getValues.forEach((item: any, index: number) => {
-        const { type, label, name } = item
-        formArr.push({
-          type,
-          label,
-          name,
+        getValues.forEach((item: any, index: number) => {
+          const { type, label, name } = item
+          formArr.push({
+            type,
+            label,
+            name,
+          })
+
+          if (type === 'text') {
+            formState[key][getKeys[index]] = ''
+          } else if (type === 'number') {
+            formState[key][getKeys[index]] = 0
+          }
         })
-
-        if (type === 'text') {
-          formState[key][getKeys[index]] = ''
-        } else if (type === 'number') {
-          formState[key][getKeys[index]] = 0
-        }
-      })
+      }
     }
   })
   return [formArr, formState]
