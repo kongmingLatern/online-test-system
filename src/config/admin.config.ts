ad0c047af4df9f1acdc55dd0de397ccd83c53f02
凤之兮原 <1577310596@qq.com>
2022-11-28 11:23:46 +0800

    feat: finish test cases

--- src/config/admin.config.ts
+++ src/config/admin.config.ts
@@ -2,8 +2,36 @@ export default {
   form: {
     teacher: {
       teacherName: {
+        name: 'teacherName',
         type: 'text',
         label: '教师姓名',
+      },
+    },
+    task: {
+      subjectName: {
+        name: 'subjectName',
+        type: 'text',
+        label: '科目名称',
+      },
+      baseTitle: {
+        name: 'baseTitle',
+        type: 'text',
+        label: '题库名称',
+      },
+      baseName: {
+        name: 'baseName',
+        type: 'text',
+        label: '题目名称',
+      },
+      taskTerm: {
+        name: 'taskTerm',
+        type: 'text',
+        label: '考试学期',
+      },
+      taskTime: {
+        name: 'taskTime',
+        type: 'number',
+        label: '考试时间',
       },
     },
   },
