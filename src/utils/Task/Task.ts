71faa0b43d96ea93e5a302a25321af92a843d384
凤之兮原 <1577310596@qq.com>
2022-11-28 22:42:14 +0800

    fix: params ts error

--- src/utils/Task/Task.ts
+++ src/utils/Task/Task.ts
@@ -2,13 +2,13 @@ export default class Task {
   public baseTitle: string = ''
   public subjectName: string | undefined = ''
   public teacherName: string | undefined = ''
-  public createUser: string
+  public createUser: string | undefined
 
   constructor(
     baseTitle: string,
-    subjectName: string | undefined,
-    teacherName: string | undefined,
-    createUser: string
+    subjectName?: string | undefined,
+    teacherName?: string | undefined,
+    createUser?: string | undefined
   ) {
     this.baseTitle = baseTitle
     this.subjectName = subjectName
