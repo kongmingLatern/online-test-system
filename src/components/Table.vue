522617f9ba32d2f36373c82e9a8880a63787b1f4
凤之兮原 <1577310596@qq.com>
2022-11-28 21:45:52 +0800

    enhance Table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -36,7 +36,13 @@
       <template v-if="column.dataIndex === 'deleteAndAuth'">
         <a-space>
           <a-button type="danger" rounded>授权</a-button>
-          <a-button type="danger" rounded>删除</a-button>
+          <a-button
+            type="danger"
+            rounded
+            @click="deleteItem(record)"
+          >
+            删除
+          </a-button>
         </a-space>
       </template>
       <template v-if="column.dataIndex === 'student'">
