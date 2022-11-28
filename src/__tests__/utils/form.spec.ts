5977092bff67e3c670c5720946eb700162df3179
凤之兮原 <1577310596@qq.com>
2022-11-28 11:16:07 +0800

    enhance: change return

---
+++ src/__tests__/utils/form.spec.ts
@@ -0,0 +1,216 @@
+import getFormItem from '@/utils/form'
+
+describe('test form', () => {
+  it('have an only object', () => {
+    const data = {
+      form: {
+        teacher: {
+          teacherName: {
+            name: 'teacherName',
+            type: 'text',
+            label: '教师姓名',
+          },
+        },
+      },
+    }
+    expect(getFormItem(data)).toEqual([
+      [
+        {
+          name: 'teacherName',
+          type: 'text',
+          label: '教师姓名',
+        },
+      ],
+      {
+        teacher: {
+          teacherName: '',
+        },
+      },
+    ])
+  })
+  it('have two object', () => {
+    const data = {
+      form: {
+        teacher: {
+          teacherName: {
+            name: 'teacherName',
+            type: 'text',
+            label: '教师姓名',
+          },
+          teacherNo: {
+            name: 'teacherNo',
+            type: 'text',
+            label: '教工号',
+          },
+        },
+      },
+    }
+    expect(getFormItem(data)).toEqual([
+      [
+        {
+          name: 'teacherName',
+          type: 'text',
+          label: '教师姓名',
+        },
+        {
+          name: 'teacherNo',
+          type: 'text',
+          label: '教工号',
+        },
+      ],
+      {
+        teacher: {
+          teacherName: '',
+          teacherNo: '',
+        },
+      },
+    ])
+  })
+  it('have some object params', () => {
+    const data = {
+      form: {
+        teacher: {
+          teacherName: {
+            name: 'teacherName',
+            type: 'text',
+            label: '教师姓名',
+          },
+          teacherNo: {
+            name: 'teacherNo',
+            type: 'text',
+            label: '教工号',
+          },
+          isAuth: {
+            name: 'isAuth',
+            type: 'text',
+            label: '是否授权',
+          },
+        },
+      },
+    }
+    expect(getFormItem(data)).toEqual([
+      [
+        {
+          name: 'teacherName',
+          type: 'text',
+          label: '教师姓名',
+        },
+        {
+          name: 'teacherNo',
+          type: 'text',
+          label: '教工号',
+        },
+        {
+          name: 'isAuth',
+          type: 'text',
+          label: '是否授权',
+        },
+      ],
+      {
+        teacher: {
+          teacherName: '',
+          teacherNo: '',
+          isAuth: '',
+        },
+      },
+    ])
+  })
+  it('test type is number', () => {
+    const data = {
+      form: {
+        task: {
+          taskName: {
+            name: 'taskName',
+            type: 'text',
+            label: '考试名称',
+          },
+          taskTime: {
+            name: 'taskTime',
+            type: 'number',
+            label: '考试时长',
+          },
+        },
+      },
+    }
+    expect(getFormItem(data)).toEqual([
+      [
+        {
+          name: 'taskName',
+          type: 'text',
+          label: '考试名称',
+        },
+        {
+          name: 'taskTime',
+          type: 'number',
+          label: '考试时长',
+        },
+      ],
+      {
+        task: {
+          taskName: '',
+          taskTime: 0,
+        },
+      },
+    ])
+  })
+  it.skip('test type is number', () => {
+    const data = {
+      form: {
+        task: {
+          taskName: {
+            name: 'taskName',
+            type: 'text',
+            label: '考试名称',
+          },
+          taskTime: {
+            name: 'taskTime',
+            type: 'number',
+            label: '考试时长',
+          },
+        },
+        teahcer: {
+          teacherName: {
+            name: 'teacherName',
+            type: 'text',
+            label: '教师姓名',
+          },
+          teacherNo: {
+            name: 'teacherNo',
+            type: 'text',
+            label: '教工号',
+          },
+        },
+      },
+    }
+    expect(getFormItem(data)).toEqual([
+      [
+        {
+          type: 'text',
+          label: '考试名称',
+        },
+        {
+          type: 'number',
+          label: '考试时长',
+        },
+        {
+          type: 'text',
+          label: '教师姓名',
+        },
+        {
+          type: 'text',
+          label: '教工号',
+        },
+      ],
+      {
+        task: {
+          taskName: '',
+          taskTime: 0,
+        },
+        teahcer: {
+          teacherName: '',
+          teacherNo: '',
+        },
+      },
+    ])
+  })
+})
