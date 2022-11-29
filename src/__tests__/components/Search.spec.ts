3ed3ab2b72bca0cba56796926b7ad20a700de0fb
凤之兮原 <1577310596@qq.com>
2022-11-29 19:33:10 +0800

    feat: add test cases about Search

---
+++ src/__tests__/components/Search.spec.ts
@@ -0,0 +1,13 @@
+import {
+  findComponent,
+  registGlobalComponent,
+} from '@/utils'
+import Search from '@/components/Search.vue'
+
+const wrapper = registGlobalComponent(Search)
+describe('Search shoule be exists', () => {
+  it('should be exists', () => {
+    const search = findComponent(wrapper, 'search')
+    expect(search.exists()).toBe(true)
+  })
+})
