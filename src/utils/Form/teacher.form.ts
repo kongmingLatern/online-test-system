fe3cce32d67557e8803aa483be8cabb2fefd50d7
凤之兮原 <1577310596@qq.com>
2022-11-29 16:23:18 +0800

    chore: create ts to store teacher form

---
+++ src/utils/Form/teacher.form.ts
@@ -0,0 +1,17 @@
+export default {
+  teacherNo: {
+    name: 'teacherNo',
+    type: 'text',
+    label: '教工号',
+  },
+  teacherName: {
+    name: 'teacherName',
+    type: 'text',
+    label: '教师姓名',
+  },
+  isAuth: {
+    name: 'isAuth',
+    type: 'number',
+    label: '是否授权',
+  },
+}
