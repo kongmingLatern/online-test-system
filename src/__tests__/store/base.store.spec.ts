810a11c294c97ad46d1b768e6c5dda9cc33d27ab
凤之兮原 <1577310596@qq.com>
2022-11-29 21:43:18 +0800

    feat: add test api snap to base.store

---
+++ src/__tests__/store/base.store.spec.ts
@@ -0,0 +1,43 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useTask } from '@/stores/task.store'
+import { useBase } from '@/stores/base.store'
+
+describe('Task Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  // NOTE: 测试接口 'base/pages' 的数据'
+  it.skip('should return 1 record', async () => {
+    const store = useTask()
+    await expect(store.getTaskByCurrentPage(1, 1)).resolves
+      .toMatchInlineSnapshot(`
+      [
+        [
+          Task {
+            "baseTitle": "123",
+            "createUser": null,
+            "subjectName": "高数A",
+            "teacherName": "沈金晶",
+          },
+        ],
+        1,
+      ]
+    `)
+  })
+  // NOTE: 测试接口 'base/list' 的数据'
+  it.skip('should return 1 record', async () => {
+    const store = useBase()
+    await expect(store.getBaseList('1595617742638100481'))
+      .resolves.toMatchInlineSnapshot(`
+      [
+        {
+          "baseId": "1595984141143273474",
+          "baseTitle": "123",
+          "createUser": null,
+          "subjectId": null,
+        },
+      ]
+    `)
+  })
+})
