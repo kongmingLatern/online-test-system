c5512947b51bc14612aa0322570cd92f9a839f97
凤之兮原 <1577310596@qq.com>
2022-11-29 15:21:23 +0800

    feat: add column config

--- src/config/admin.config.ts
+++ src/config/admin.config.ts
@@ -129,9 +129,9 @@ export default {
     },
   },
   column: {
-    judge: [
+    radio: [
       {
-        name: '题目编号',
+        title: '题目编号',
         dataIndex: 'no',
         options: {
           width: 100,
@@ -139,10 +139,140 @@ export default {
         },
       },
       {
-        name: '题目',
+        title: '题目',
         dataIndex: 'questionList',
         options: {
+          width: 200,
           ellipsis: true,
+          align: 'center',
+        },
+      },
+      {
+        title: '选项',
+        dataIndex: 'questionAnswer',
+        options: {
+          width: 250,
+          align: 'center',
+        },
+      },
+      {
+        title: '答案',
+        dataIndex: 'questionCorrect',
+        options: {
+          width: 150,
+          ellipsis: true,
+          align: 'center',
+        },
+      },
+      {
+        title: '所属试题名称',
+        dataIndex: 'baseTitle',
+        options: {
+          width: 100,
+          align: 'center',
+        },
+      },
+      {
+        title: '操作',
+        dataIndex: 'delete',
+        options: {
+          width: 100,
+          align: 'center',
+        },
+      },
+    ],
+    checkbox: [
+      {
+        title: '题目编号',
+        dataIndex: 'no',
+        options: {
+          width: 100,
+          align: 'center',
+        },
+      },
+      {
+        title: '题目',
+        dataIndex: 'questionList',
+        options: {
+          width: 200,
+          ellipsis: true,
+          align: 'center',
+        },
+      },
+      {
+        title: '选项',
+        dataIndex: 'questionAnswer',
+        options: {
+          width: 250,
+          align: 'center',
+        },
+      },
+      {
+        title: '答案',
+        dataIndex: 'questionCorrect',
+        options: {
+          width: 150,
+          ellipsis: true,
+          align: 'center',
+        },
+      },
+      {
+        title: '所属试题名称',
+        dataIndex: 'baseTitle',
+        options: {
+          width: 100,
+          align: 'center',
+        },
+      },
+      {
+        title: '操作',
+        dataIndex: 'delete',
+        options: {
+          width: 100,
+          align: 'center',
+        },
+      },
+    ],
+    judge: [
+      {
+        title: '题目编号',
+        dataIndex: 'no',
+        options: {
+          width: 50,
+          align: 'center',
+        },
+      },
+      {
+        title: '题目',
+        dataIndex: 'questionList',
+        options: {
+          width: 200,
+          ellipsis: true,
+          align: 'center',
+        },
+      },
+      {
+        title: '答案',
+        dataIndex: 'questionCorrect',
+        options: {
+          width: 50,
+          ellipsis: true,
+          align: 'center',
+        },
+      },
+      {
+        title: '所属试题名称',
+        dataIndex: 'baseTitle',
+        options: {
+          width: 100,
+          align: 'center',
+        },
+      },
+      {
+        title: '操作',
+        dataIndex: 'delete',
+        options: {
+          width: 50,
           align: 'center',
         },
       },
