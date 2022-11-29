d7a1301876888f8e38176349d67eda419b373fd5
凤之兮原 <1577310596@qq.com>
2022-11-29 21:43:28 +0800

    feat: add test api snap to subject.store

---
+++ src/__tests__/store/subject.store.spec.ts
@@ -0,0 +1,23 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useSubject } from '@/stores/subject.store'
+
+describe('Subject Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  // NOTE: 测试接口 '/subject/list' 的数据'
+  it.skip('should return 1 record', async () => {
+    const store = useSubject()
+    await expect(store.getSubjectList()).resolves
+      .toMatchInlineSnapshot(`
+      [
+        {
+          "subjectId": "1595617742638100481",
+          "subjectName": "高数A",
+          "teacherId": null,
+        },
+      ]
+    `)
+  })
+})
