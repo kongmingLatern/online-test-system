d71d682644207596ea2298bcc1f6bd14730a6433
凤之兮原 <1577310596@qq.com>
2022-11-30 17:30:49 +0800

    fix: fix api test error and upgrade it

--- src/__tests__/store/grade.store.spec.ts
+++ src/__tests__/store/grade.store.spec.ts
@@ -7,7 +7,7 @@ describe('Radio Store', () => {
   })
 
   // NOTE: 测试接口 'student/pages' 的数据'
-  it.skip('should return 8 record', async () => {
+  it('should return 8 record', async () => {
     const store = useGrade()
     await expect(store.getGradeByCurrentPage(1, 1)).resolves
       .toMatchInlineSnapshot(`
@@ -15,14 +15,15 @@ describe('Radio Store', () => {
         [
           Grade {
             "baseTitle": "123",
-            "classNo": "沈金晶",
-            "grade": 99,
-            "studentName": "0922201",
-            "studentNo": "092218114",
+            "classNo": "0922201",
+            "grade": 0,
+            "studentId": "092218114",
+            "studentName": "沈金晶",
+            "studentNo": "0922201",
             "subjectName": "高数A",
           },
         ],
-        54,
+        2,
       ]
     `)
   })
