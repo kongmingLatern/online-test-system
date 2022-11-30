58f354fe2d4bf0de3690aae943638ab727fda75a
凤之兮原 <1577310596@qq.com>
2022-11-30 17:32:55 +0800

    change: change width about taskStartToEnd

--- src/utils/TableData/SearchStudentPassword.data.ts
+++ src/utils/TableData/SearchStudentPassword.data.ts
@@ -1,45 +1,3 @@
-// import type { TableColumnsOptions } from '@/type'
-// import { Column } from '../Task'
-
-// const options: Partial<TableColumnsOptions> = {
-//   align: 'center',
-// }
-// export const columns = [
-//   new Column('考试编号', 'no', 'no', {
-//     ...options,
-//     width: 100,
-//   }),
-//   new Column('考试科目', 'subjectName', 'subjectName', {
-//     ...options,
-//     ellipsis: true,
-//   }),
-//   new Column(
-//     '考试密码',
-//     'taskPassword',
-//     'taskPassword',
-//     options
-//   ),
-//   new Column('考试时间', 'taskTime', 'taskTime', {
-//     ...options,
-//     width: 150,
-//   }),
-//   new Column('班级', 'classNo', 'classNo', {
-//     ...options,
-//     width: 200,
-//   }),
-//   new Column('学号', 'studentNo', 'studentNo', {
-//     ...options,
-//     width: 200,
-//   }),
-//   new Column('姓名', 'studentName', 'studentName', {
-//     ...options,
-//     width: 200,
-//   }),
-//   new Column('考试状态', 'taskStutus', 'taskStutus', {
-//     ...options,
-//     width: 200,
-//   }),
-// ]
 export const columns = [
   {
     title: '考试编号',
@@ -69,7 +27,7 @@ export const columns = [
     dataIndex: 'taskStartToEnd',
     options: {
       align: 'center',
-      width: 150,
+      width: 250,
     },
   },
   {
