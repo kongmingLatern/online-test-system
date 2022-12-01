6b969ceb7d425038e76149bd42be3a7792278f44
凤之兮原 <1577310596@qq.com>
2022-12-01 20:33:52 +0800

    feat: finish test cases

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { reactive } from 'vue'
+
 export function setColumn(
   title: string,
   name: string,
@@ -40,9 +42,47 @@ export function reactiveToCommon(
 ): any[] {
   const result: any[] = []
   const target = arr.question
-  const { questionCorrect } = target
-  questionCorrect.forEach(item => {
-    result.push(item.values)
+  const { questionCorrectList, questionListAnswerList } =
+    target
+  if (questionCorrectList) {
+    questionCorrectList.forEach(item => {
+      result.push(item.values)
+    })
+  } else if (questionListAnswerList) {
+    questionListAnswerList.forEach(item => {
+      result.push(item.values)
+    })
+  }
+  return result
+}
+
+export function setReactiveValue(arr: any[]): any[] {
+  const result = reactive<any[]>([])
+  arr.forEach(obj => {
+    if (typeof obj === 'object') {
+      for (const key in obj) {
+        if (
+          Object.prototype.hasOwnProperty.call(obj, key)
+        ) {
+          if (key === 'baseTitle') {
+            const element = obj[key]
+            obj.value = element
+            result.push(obj)
+            break
+          }
+        }
+      }
+    }
   })
   return result
+}
+
+export const getValueByObject = (formState, key) => {
+  const obj: any[] = []
+  const arr = formState[key]
+  console.log(arr)
+  arr.forEach(item => {
+    obj.push(item.values)
+  })
+  return obj
 }
