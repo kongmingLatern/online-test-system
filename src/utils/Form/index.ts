3d42dd502875fafc93d1c2e35dca1eb9f774f596
凤之兮原 <1577310596@qq.com>
2022-12-01 18:04:25 +0800

    enhance: enhance Form/index.ts

--- src/utils/Form/index.ts
+++ src/utils/Form/index.ts
@@ -1,18 +1,14 @@
 import TeacherForm from './teacher'
 import TaskForm from './task'
 import StudentForm from './student'
-import RadioForm from './radio'
-import CheckboxForm from './checkbox'
-import JudgeForm from './judge'
+import QuestionForm from './radio'
 
 // 表单配置组件 => FormItem.vue
 const formConfig = {
   teacher: TeacherForm,
   task: TaskForm,
   student: StudentForm,
-  radio: RadioForm,
-  checkbox: CheckboxForm,
-  judge: JudgeForm,
+  question: QuestionForm,
 }
 
 export { formConfig }
