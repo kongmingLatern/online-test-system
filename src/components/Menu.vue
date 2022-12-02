f40b3d127af48d91ebf0a138fd24e3d91df5dd48
凤之兮原 <1577310596@qq.com>
2022-12-02 20:43:32 +0800

    feat: finish Menu test case

---
+++ src/components/Menu.vue
@@ -0,0 +1,54 @@
+<template>
+  <a-menu
+  data-test="menu"
+    v-model:selectedKeys="current"
+    mode="horizontal"
+    class="menu"
+  >
+    <a-menu-item key="index">
+      <template #icon>
+        <HomeOutlined />
+      </template>
+      首页
+    </a-menu-item>
+    <a-menu-item key="task">
+      <template #icon>
+        <PaperClipOutlined />
+      </template>
+      试题库
+    </a-menu-item>
+    <a-menu-item key="grade">
+      <template #icon>
+        <FileSyncOutlined />
+      </template>
+      成绩管理
+    </a-menu-item>
+    <a-menu-item key="personal">
+      <template #icon>
+        <UserOutlined />
+      </template>
+      我的考试
+    </a-menu-item>
+  </a-menu>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {
+  HomeOutlined,
+  PaperClipOutlined,
+  FileSyncOutlined,
+  UserOutlined,
+} from '@ant-design/icons-vue'
+const current = ref<string[]>(['index'])
+</script>
+
+<style scoped>
+.menu {
+  height: 64px;
+  padding: 0 50px;
+  /* color: #fff; */
+  /* color: rgba(0, 0, 0, 0.85); */
+  line-height: 64px;
+  background: #001529;
+}
+</style>
