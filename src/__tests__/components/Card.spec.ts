cad50a50c75283cdec76976286cd64ee8c776963
凤之兮原 <1577310596@qq.com>
2022-12-02 20:40:06 +0800

    feat: add Card test case

---
+++ src/__tests__/components/Card.spec.ts
@@ -0,0 +1,19 @@
+import {
+  findComponent,
+  registGlobalComponent,
+} from '@/utils'
+
+import Card from '@/components/Card.vue'
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = registGlobalComponent(Card)
+})
+
+describe('Card shoule be existed', () => {
+  it('shoule be exists', () => {
+    const card = findComponent(wrapper, 'card')
+    expect(card.exists()).toBeTruthy()
+  })
+})
