74079c2cf60bc3e87782f822003db0ed239ca709
凤之兮原 <1577310596@qq.com>
2022-12-02 21:34:46 +0800

    feat: finish carousel test cases

---
+++ src/components/Carousel.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-carousel :after-change="onChange" data-test="carousel">
+    <div><h3>1</h3></div>
+    <div><h3>2</h3></div>
+    <div><h3>3</h3></div>
+    <div><h3>4</h3></div>
+  </a-carousel>
+</template>
+<script lang="ts" setup>
+const onChange = (current: number) => {
+  console.log(current)
+}
+</script>
+<style scoped>
+.ant-carousel :deep(.slick-slide) {
+  text-align: center;
+  height: 160px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
+
+.ant-carousel :deep(.slick-slide h3) {
+  color: #fff;
+}
+</style>
