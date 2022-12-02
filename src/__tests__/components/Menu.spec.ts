70182128f258fd7b0c3ed4f3f9705bd1769a5a09
凤之兮原 <1577310596@qq.com>
2022-12-02 20:43:01 +0800

    feat: add Menu test case

---
+++ src/__tests__/components/Menu.spec.ts
@@ -0,0 +1,19 @@
+import {
+  findComponent,
+  registGlobalComponent,
+} from '@/utils'
+
+import Menu from '@/components/Menu.vue'
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = registGlobalComponent(Menu)
+})
+
+describe('Menu shoule be existed', () => {
+  it('shoule be exists', () => {
+    const menu = findComponent(wrapper, 'menu')
+    expect(menu.exists()).toBeTruthy()
+  })
+})
