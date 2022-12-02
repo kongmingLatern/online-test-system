8d68d94b9dd32dca3b8913c60b73474148ad20bf
凤之兮原 <1577310596@qq.com>
2022-12-02 22:27:26 +0800

    feat: add List

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-list bordered :data-source="data">
+    <template #renderItem="{ item, index }">
+      <a-list-item>
+        <a-tag>
+          <span mr-3 rounded>{{ index + 1 }}</span>
+          <a-tag color="red" text-sm>{{ item }}</a-tag>
+        </a-tag>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+const data: string[] = [
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+]
+</script>
+
+<style scoped></style>
