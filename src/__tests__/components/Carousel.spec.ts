f722db934dfe16221388b8bed98dfea65e0b01d7
凤之兮原 <1577310596@qq.com>
2022-12-02 21:33:09 +0800

    feat: add Carousel test case

---
+++ src/__tests__/components/Carousel.spec.ts
@@ -0,0 +1,19 @@
+import {
+  findComponent,
+  registGlobalComponent,
+} from '@/utils'
+
+import Carousel from '@/components/Carousel.vue'
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = registGlobalComponent(Carousel)
+})
+
+describe('Card shoule be existed', () => {
+  it('shoule be exists', () => {
+    const carousel = findComponent(wrapper, 'carousel')
+    expect(carousel.exists()).toBeTruthy()
+  })
+})
