21d2bf94eb85e33157e06239582bd68e5aa1ef28
凤之兮原 <1577310596@qq.com>
2022-12-02 20:38:46 +0800

    chore: use dark-css

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from './router'
 import Antd from 'ant-design-vue'
 
 import 'ant-design-vue/dist/antd.css'
+import 'ant-design-vue/dist/antd.dark.css'
 import 'uno.css'
 import '@/assets/global.css'
 
