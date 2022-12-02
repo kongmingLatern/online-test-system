4579ad20be53e0b8e4548bf268472e0640e0e251
凤之兮原 <1577310596@qq.com>
2022-12-02 21:35:31 +0800

    feat: finish test cases

---
+++ src/pages/home/Personal.vue
@@ -0,0 +1,52 @@
+<template>
+  <Card :cardList="cardList" data-test="personal" />
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const cardList = reactive([
+  {
+    title: '高等数学A',
+    taskTime: '2022年9月20日 09:00',
+    limitTime: '120',
+  },
+  {
+    title: '高等数学B',
+    taskTime: '2022年9月22日 19:00',
+    limitTime: '20',
+  },
+  {
+    title: '高等数学A',
+    taskTime: '2022年9月20日 09:00',
+    limitTime: '120',
+  },
+  {
+    title: '高等数学B',
+    taskTime: '2022年9月22日 19:00',
+    limitTime: '20',
+  },
+  {
+    title: '高等数学A',
+    taskTime: '2022年9月20日 09:00',
+    limitTime: '120',
+  },
+  {
+    title: '高等数学B',
+    taskTime: '2022年9月22日 19:00',
+    limitTime: '20',
+  },
+  {
+    title: '高等数学A',
+    taskTime: '2022年9月20日 09:00',
+    limitTime: '120',
+  },
+  {
+    title: '高等数学B',
+    taskTime: '2022年9月22日 19:00',
+    limitTime: '20',
+  },
+])
+</script>
+
+<style scoped></style>
