cbb66692c02a5410808ecfbf855f887e4ffed770
凤之兮原 <1577310596@qq.com>
2022-12-02 22:27:16 +0800

    chore: add css

--- src/pages/home/HomeIndex.vue
+++ src/pages/home/HomeIndex.vue
@@ -1,14 +1,16 @@
 <template>
   <!-- NOTE: 轮播图 -->
   <Carousel />
-  <div flex="~">
-    <Rules />
-    <Rules />
+  <div flex="~" justify="around" p-5>
+      <Rules />
+      <Recent ml-5/>
   </div>
 </template>
 
 <script setup lang="ts">
 import Rules from '@/views/home/Rules.vue'
+import Recent from '@/views/home/Recent.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
