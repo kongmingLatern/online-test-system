18ac35238f018a9f1aee2c7006b458d6e5f25b52
凤之兮原 <1577310596@qq.com>
2022-12-02 21:55:35 +0800

    chore: add index

--- src/pages/home/HomeIndex.vue
+++ src/pages/home/HomeIndex.vue
@@ -1,7 +1,14 @@
 <template>
-  <Card />
+  <!-- NOTE: 轮播图 -->
+  <Carousel />
+  <div flex="~">
+    <Rules />
+    <Rules />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Rules from '@/views/home/Rules.vue'
+</script>
 
 <style scoped></style>
