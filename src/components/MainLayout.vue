a99c23e9c93c7ff5481df11308abfb82620d7efc
凤之兮原 <1577310596@qq.com>
2022-12-02 20:44:24 +0800

    feat: finish MainLayout test cases

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-layout data-test="layout">
+    <a-layout-header flex>
+      <Header>
+        <template #menu>
+          <Menu />
+        </template>
+      </Header>
+    </a-layout-header>
+    <a-layout-content>
+      <router-view />
+    </a-layout-content>
+    <a-layout-footer>
+      <slot name="footer"></slot>
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
