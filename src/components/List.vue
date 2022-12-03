a0743ec171d405cb412192607fe35940033c7271
凤之兮原 <1577310596@qq.com>
2022-12-03 22:35:34 +0800

    format

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,9 @@
 <template>
   <a-list bordered :data-source="data">
     <template #renderItem="{ item, index }">
-      <a-list-item hover="bg-red color-white">
+      <a-list-item
+        hover="bg-red-800 color-white ease-out duration-300"
+      >
         <div>
           <span mr-2 text-sm>{{ index + 1 }}</span>
           <a-tag color="red" text-sm>{{ item }}</a-tag>
@@ -13,12 +15,12 @@
 
 <script setup lang="ts">
 const data: string[] = [
-  "高等数学A 2021-06-01 08:00",
-  "高等数学A 2021-06-01 08:00",
-  "高等数学A 2021-06-01 08:00",
-  "高等数学A 2021-06-01 08:00",
-  "高等数学A 2021-06-01 08:00",
-];
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+  '高等数学A 2021-06-01 08:00',
+]
 </script>
 
 <style scoped></style>
