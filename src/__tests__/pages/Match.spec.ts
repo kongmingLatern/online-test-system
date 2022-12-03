e8d40b17dde21c7fe164d02ca6dcb684763b309c
凤之兮原 <1577310596@qq.com>
2022-12-03 20:45:50 +0800

    feat: add Match test cases

---
+++ src/__tests__/pages/Match.spec.ts
@@ -0,0 +1,14 @@
+import { beforeEach } from 'vitest'
+import { findComponent } from '../../utils/vueUtils'
+import Match from '@/pages/home/Match.vue'
+import { registGlobalComponent } from '@/utils'
+let wrapper
+beforeEach(() => {
+  wrapper = registGlobalComponent(Match)
+})
+describe('it shoule be exist', () => {
+  it('shoule be existed', () => {
+    const match = findComponent(wrapper, 'match')
+    expect(match).toBeTruthy()
+  })
+})
