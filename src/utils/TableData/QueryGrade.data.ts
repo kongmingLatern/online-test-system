6bac4a45de38f7f7834defa59f00cfeadd4262a7
凤之兮原 <1577310596@qq.com>
2022-12-03 21:17:50 +0800

    feat: add new TableData

---
+++ src/utils/TableData/QueryGrade.data.ts
@@ -0,0 +1,25 @@
+const options = {
+  align: 'center',
+}
+export const columns = [
+  {
+    title: '科目',
+    dataIndex: 'subjectName',
+    options,
+  },
+  {
+    title: '考试名称',
+    dataIndex: 'taskName',
+    options,
+  },
+  {
+    title: '考试时间',
+    dataIndex: 'taskTime',
+    options,
+  },
+  {
+    title: '成绩',
+    dataIndex: 'grade',
+    options,
+  },
+]
--- src/utils/TableData/index.ts
+++ src/utils/TableData/index.ts
@@ -1,17 +1,18 @@
-import { columns as TaskColumn } from "./Task.data";
-import { columns as QuestionColumn } from "./Question.data.";
+import { columns as TaskColumn } from './Task.data'
+import { columns as QuestionColumn } from './Question.data.'
 
-import { columns as JudgeColumn } from "./Judge.data";
+import { columns as JudgeColumn } from './Judge.data'
 
 import {
   uploadColumns as StudentUploadColumn,
   infoColumns as StudentInfoColumns,
-} from "./Student.data";
+} from './Student.data'
 
-import { columns as GradeColumn } from "./Grade.data";
-import { columns as SearchStudentPasswordColumn } from "./SearchStudentPassword.data";
-import { columns as TeacherColumn } from "./Teacher.data";
-import { columns as TaskInfoColumn } from "./TaskInfoColumn.data";
+import { columns as GradeColumn } from './Grade.data'
+import { columns as SearchStudentPasswordColumn } from './SearchStudentPassword.data'
+import { columns as TeacherColumn } from './Teacher.data'
+import { columns as TaskInfoColumn } from './TaskInfoColumn.data'
+import { columns as QueryGrade } from './QueryGrade.data'
 
 const columnConfig = {
   studentUpload: StudentUploadColumn,
@@ -20,9 +21,10 @@ const columnConfig = {
   judge: JudgeColumn,
   task: TaskColumn,
   grade: GradeColumn,
+  queryGrade: QueryGrade,
   searchPassword: SearchStudentPasswordColumn,
   teacher: TeacherColumn,
   taskInfo: TaskInfoColumn,
-};
+}
 
-export { columnConfig };
+export { columnConfig }
