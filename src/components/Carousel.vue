40be29dc6fe2c0044963bd736fce8f96184e1253
凤之兮原 <1577310596@qq.com>
2022-12-03 14:22:31 +0800

    feat: finish Carousel test cases

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,12 +1,50 @@
 <template>
-  <a-carousel :after-change="onChange" data-test="carousel">
-    <div><h3>1</h3></div>
-    <div><h3>2</h3></div>
-    <div><h3>3</h3></div>
-    <div><h3>4</h3></div>
+  <a-carousel
+    arrows
+    :after-change="onChange"
+    data-test="carousel"
+    mt-10
+  >
+    <template #prevArrow>
+      <div
+        class="custom-slick-arrow"
+        style="left: 10px; z-index: 1"
+      >
+        <left-circle-outlined />
+      </div>
+    </template>
+    <template #nextArrow>
+      <div class="custom-slick-arrow" style="right: 10px">
+        <right-circle-outlined />
+      </div>
+    </template>
+    <div>
+      <img
+        src="https://img2.baidu.com/it/u=505379311,4113824194&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=250"
+      />
+    </div>
+    <div>
+      <img
+        src="https://img2.baidu.com/it/u=505379311,4113824194&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=250"
+      />
+    </div>
+    <div>
+      <img
+        src="https://img2.baidu.com/it/u=505379311,4113824194&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=250"
+      />
+    </div>
+    <div>
+      <img
+        src="https://img2.baidu.com/it/u=505379311,4113824194&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=250"
+      />
+    </div>
   </a-carousel>
 </template>
 <script lang="ts" setup>
+import {
+  LeftCircleOutlined,
+  RightCircleOutlined,
+} from '@ant-design/icons-vue'
 const onChange = (current: number) => {
   console.log(current)
 }
@@ -14,13 +52,20 @@ const onChange = (current: number) => {
 <style scoped>
 .ant-carousel :deep(.slick-slide) {
   text-align: center;
-  height: 160px;
-  line-height: 160px;
+  height: 250px;
+  line-height: 250px;
   background: #364d79;
   overflow: hidden;
 }
 
 .ant-carousel :deep(.slick-slide h3) {
   color: #fff;
+}
+.ant-carousel {
+  margin-top: 10px;
+}
+img {
+  width: 100%;
+  height: 250px;
 }
 </style>
