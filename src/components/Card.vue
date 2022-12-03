a0463ea34e6bbcbdfab9026ae557d8360d524c47
凤之兮原 <1577310596@qq.com>
2022-12-03 18:21:43 +0800

    enhance: enhance Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -32,7 +32,7 @@
                 class="button-green"
                 w-60
                 rounded="full"
-                @click="goTask(item.matchId)"
+                @click="showModal(item.matchId)"
               >
                 进入考试
               </a-button>
@@ -45,8 +45,12 @@
       <template #modalContent>
         <Rules class="font" text="lg" px-10>
           <template #matchInfo>
-            <p text="center xl">当前考试：高等数学 A</p>
-            <p text="center xl">考试时间： 2022年9月12日 08:00 - 10:00</p>
+            <p text="center xl">
+              当前考试：{{ currentMatch.title }}
+            </p>
+            <p text="center xl">
+              考试时间：{{ currentMatch.taskTime }}
+            </p>
             <a-divider />
           </template>
         </Rules>
@@ -60,15 +64,23 @@
               placeholder="请输入考试码"
             />
           </div>
-          <p>如由于自身操作不当导致系统故障，不予提供二次考试密码。</p>
+          <p>
+            如由于自身操作不当导致系统故障，不予提供二次考试密码。
+          </p>
           <p>
             如系统发生意外，请联系监考老师，由监考老师确定为系统故障后，再由监考老师联系管理员提供二次考试密码并继续考试。
           </p>
           <a-radio text-lg font-semibold>
             我已阅读上述准则，并且保证严格遵守
           </a-radio>
-          <footer text-right>
-            <a-button class="button-green" h-10 w-30 rounded>
+          <footer text-center mt-10>
+            <a-button
+              class="button-green"
+              h-15
+              w-60
+              rounded
+              @click="goTask(currentMatch.matchId)"
+            >
               进入考试
             </a-button>
           </footer>
@@ -79,28 +91,47 @@
 </template>
 
 <script setup lang="ts">
-import router from "@/router";
-import Rules from "@/views/home/Rules.vue";
-import { provide, ref } from "vue";
+import router from '@/router'
+import Rules from '@/views/home/Rules.vue'
+import { provide, reactive, ref } from 'vue'
 
-defineProps<{
-  cardList?: any[];
-}>();
-const comShow = ref<boolean>(false);
+const props = defineProps<{
+  cardList?: any[]
+}>()
+const comShow = ref<boolean>(false)
+const currentMatch = reactive<Record<string, any>>({})
 
-provide("title", "进入考试");
-provide("comShow", comShow);
-const taskPassword = ref<string>("");
+provide('title', '进入考试')
+provide('comShow', comShow)
+const taskPassword = ref<string>('')
 
-const goTask = (matchId) => {
-  comShow.value = true;
-  // router.push({
-  //   path: '/match',
-  //   query: {
-  //     matchId,
-  //   },
-  // })
-};
+const showModal = matchId => {
+  comShow.value = true
+  // NOTE: 获取到当前的考试信息
+  props.cardList!.find(item => {
+    if (item.matchId === matchId) {
+      for (const key in item) {
+        if (
+          Object.prototype.hasOwnProperty.call(item, key)
+        ) {
+          const element = item[key]
+          currentMatch[key] = element
+        }
+      }
+    }
+  })
+  console.log(currentMatch)
+}
+const goTask = matchId => {
+  // console.log(matchId)
+
+  router.push({
+    path: '/match',
+    query: {
+      matchId,
+    },
+  })
+}
 </script>
 
 <style scoped>
@@ -119,6 +150,6 @@ img {
   width: 100%;
 }
 .font {
-  font-family: "仿宋";
+  font-family: '仿宋';
 }
 </style>
