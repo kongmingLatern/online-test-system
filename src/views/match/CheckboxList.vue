2915f08fa03fa4d172e22c3025e641a9e72fffb7
凤之兮原 <1577310596@qq.com>
2022-12-03 20:20:33 +0800

    feat: finish CheckboxList

--- src/views/match/CheckboxList.vue
+++ src/views/match/CheckboxList.vue
@@ -1,10 +1,15 @@
 <template>
   <div v-for="(item, index) in data">
-    <p>{{ index + 1 }}.{{ item.questionList }}</p>
-    <a-checkbox-group v-model:value="value">
+    <p :id="'checkbox' + (index + 1)">
+      {{ index + 1 }}.{{ item.questionList }}
+    </p>
+    <a-checkbox-group
+      v-model:value="item.questionCorrectList"
+    >
       <a-checkbox
         v-for="(list, index) in item.questionAnswerList"
-        :value="item.questionAnswerList[index].answer"
+        :value="item.questionAnswerList[index]"
+        @change="handleClick"
       >
         {{ String.fromCharCode(index + 65) }}.
         {{ list }}
@@ -14,11 +19,12 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const value = ref<string[]>([])
 const props = defineProps<{
   data: any[]
 }>()
+const handleClick = e => {
+  console.log(props.data)
+}
 console.log('多选题', props.data)
 </script>
 
