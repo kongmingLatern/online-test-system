3bb9e8645a70eaffb80bdf2ab5cfabbbb9ebf21e
凤之兮原 <1577310596@qq.com>
2022-12-03 18:23:24 +0800

    feat: add CheckList.vue

---
+++ src/views/match/CheckboxList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="(item, index) in data">
+    <p>{{ index + 1 }}.{{ item.questionList }}</p>
+    <a-checkbox-group v-model:value="value">
+      <a-checkbox
+        v-for="(list, index) in item.questionAnswerList"
+        :value="item.questionAnswerList[index].answer"
+      >
+        {{ String.fromCharCode(index + 65) }}.
+        {{ list }}
+      </a-checkbox>
+    </a-checkbox-group>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const value = ref<string[]>([])
+const props = defineProps<{
+  data: any[]
+}>()
+console.log('多选题', props.data)
+</script>
+
+<style scoped></style>
