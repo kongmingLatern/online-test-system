149126d6a4c6b5d370a09aaf0a27f2abd947a937
凤之兮原 <1577310596@qq.com>
2022-12-03 22:50:10 +0800

    feat: add getBaseList

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,10 +1,14 @@
 import { message } from 'ant-design-vue'
 import { useBase } from '../stores/base.store'
 
-export const getBaseList = async () => {
+export const getBaseList = async (data: any[]) => {
   const store = useBase()
   try {
-    return await store.getBaseList()
+    const [res, _] = await store.getBaseList()
+    if (Array.isArray(res)) {
+      Object.assign(data, {}, res)
+    }
+    return data
   } catch (e: any) {
     message.error(e)
   }
