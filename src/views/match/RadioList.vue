e766f8185090c5e6dc13b01e7db84bea8ac2ef33
凤之兮原 <1577310596@qq.com>
2022-12-03 18:23:36 +0800

    feat: add Radio and Judge List

---
+++ src/views/match/RadioList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="(item, index) in data">
+    <p>{{ index + 1 }}.{{ item.questionList }}</p>
+    <a-radio-group v-model:value="value">
+      <a-radio
+        v-for="(list, index) in item.questionAnswerList"
+        :value="item.questionAnswerList[index].answer"
+      >
+        {{ String.fromCharCode(index + 65) }}.
+        {{ list }}
+      </a-radio>
+    </a-radio-group>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const value = ref<string>('')
+const props = defineProps<{
+  data: any[]
+}>()
+console.log('单选题', props.data)
+</script>
+
+<style scoped></style>
