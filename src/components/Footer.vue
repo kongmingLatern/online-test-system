6effad2f6017c74e3ff4cc55684778e2495589af
凤之兮原 <1577310596@qq.com>
2022-12-03 22:33:39 +0800

    feat: add Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer
+    font-sans
+    absolute
+    bottom-0
+    left-0
+    h-15
+    lh-15
+    text-sm
+    text-center
+    class="full"
+    bg-red-800
+    color-white
+  >
+    &copy;092220120 092220104 在线考试系统
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.full {
+  width: 100%;
+}
+</style>
