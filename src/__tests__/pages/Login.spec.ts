c74a675712c8f31a612d74c8370760e077d190c4
凤之兮原 <1577310596@qq.com>
2022-12-03 21:18:14 +0800

    feat: add Login.spec.ts

---
+++ src/__tests__/pages/Login.spec.ts
@@ -0,0 +1,14 @@
+import { beforeEach } from 'vitest'
+import { findComponent } from '../../utils/vueUtils'
+import { registGlobalComponent } from '@/utils'
+import Login from '@/pages/Login.vue'
+let wrapper
+beforeEach(() => {
+  wrapper = registGlobalComponent(Login)
+})
+describe('it shoule be exist', () => {
+  it('shoule be existed', () => {
+    const login = findComponent(wrapper, 'login')
+    expect(login.exists()).toBeTruthy()
+  })
+})
