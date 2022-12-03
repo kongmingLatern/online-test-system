fefb931ee96e734ac80d7fb6860ef7bc8d19dee0
凤之兮原 <1577310596@qq.com>
2022-12-03 20:45:59 +0800

    feat: add QuestionList test cases

---
+++ src/__tests__/views/QuestionList.spec.ts
@@ -0,0 +1,14 @@
+import { beforeEach } from 'vitest'
+import { findComponent } from '../../utils/vueUtils'
+import { registGlobalComponent } from '@/utils'
+import QuestionList from '@/views/match/QuestionList.vue'
+let wrapper
+beforeEach(() => {
+  wrapper = registGlobalComponent(QuestionList)
+})
+describe('it shoule be exist', () => {
+  it('shoule be existed', () => {
+    const match = findComponent(wrapper, 'questionList')
+    expect(match).toBeTruthy()
+  })
+})
