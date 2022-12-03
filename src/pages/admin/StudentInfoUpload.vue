d956259e167d9353c4cf7927fc8074ae95079d48
凤之兮原 <1577310596@qq.com>
2022-12-03 18:22:44 +0800

    feat: add isShow props to show user

--- src/pages/admin/StudentInfoUpload.vue
+++ src/pages/admin/StudentInfoUpload.vue
@@ -3,12 +3,21 @@
     <Content>
       <!-- 头部 -->
       <template #header>
-        <Header text="学生信息上传" isTitle="false" />
+        <Header
+          text="学生信息上传"
+          isTitle="false"
+          isShow="false"
+        />
       </template>
 
       <!-- 导航 -->
       <template #nav>
-        <Nav data-test="studentNav" flex="~" justify="between" mb-3>
+        <Nav
+          data-test="studentNav"
+          flex="~"
+          justify="between"
+          mb-3
+        >
           <template #breadcrumb>
             <BreadCrumb />
           </template>
@@ -51,23 +60,29 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, provide, reactive, ref } from "vue";
-import { getStudentDataByCurrentPage } from "@/api/request";
-import { finishForm, removeStudent } from "@/api/student";
-import type { Student } from "@/utils";
-let data = reactive<Student[]>([]);
-const totalPage = ref<number>();
-const current = ref<number>(1);
-const pageSize = ref<number>(10);
-const loading = ref<boolean>(false);
-const isShow = ref<boolean>(false);
-const impShow = ref<boolean>(false);
+import {
+  computed,
+  onMounted,
+  provide,
+  reactive,
+  ref,
+} from 'vue'
+import { getStudentDataByCurrentPage } from '@/api/request'
+import { finishForm, removeStudent } from '@/api/student'
+import type { Student } from '@/utils'
+let data = reactive<Student[]>([])
+const totalPage = ref<number>()
+const current = ref<number>(1)
+const pageSize = ref<number>(10)
+const loading = ref<boolean>(false)
+const isShow = ref<boolean>(false)
+const impShow = ref<boolean>(false)
 
 const pagination = computed(() => ({
   total: totalPage.value,
   current: current.value,
   pageSize: pageSize.value,
-}));
+}))
 
 onMounted(async () => {
   await getStudentDataByCurrentPage(
@@ -76,40 +91,42 @@ onMounted(async () => {
     pageSize.value,
     totalPage,
     loading
-  );
-});
+  )
+})
 
 const addStudent = () => {
-  isShow.value = true;
-};
+  isShow.value = true
+}
 
 const importStudent = () => {
-  impShow.value = true;
-};
+  impShow.value = true
+}
 
-const changePage: (pagination: any) => Promise<void> = async (pagination) => {
-  pagination.current = pagination.current;
-  current.value = pagination.current;
-  data.length = 0;
+const changePage: (
+  pagination: any
+) => Promise<void> = async pagination => {
+  pagination.current = pagination.current
+  current.value = pagination.current
+  data.length = 0
   getStudentDataByCurrentPage(
     data,
     current.value,
     pageSize.value,
     totalPage,
     loading
-  );
-};
+  )
+}
 
-provide("isShow", isShow);
-provide("impShow", impShow);
-provide("title", "添加学生");
-provide("columnSort", "studentUpload");
-provide("data", data);
-provide("loading", loading);
-provide("pagination", pagination);
-provide("change", changePage);
-provide("finish", finishForm);
-provide("removeItem", removeStudent);
+provide('isShow', isShow)
+provide('impShow', impShow)
+provide('title', '添加学生')
+provide('columnSort', 'studentUpload')
+provide('data', data)
+provide('loading', loading)
+provide('pagination', pagination)
+provide('change', changePage)
+provide('finish', finishForm)
+provide('removeItem', removeStudent)
 </script>
 
 <style scoped></style>
