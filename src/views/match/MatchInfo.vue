16cc638267f0a29a0f3c2867b666f7a56a692daa
凤之兮原 <1577310596@qq.com>
2022-12-03 20:20:53 +0800

    feat: finish MatchInfo

--- src/views/match/MatchInfo.vue
+++ src/views/match/MatchInfo.vue
@@ -12,14 +12,11 @@
     class="match-container"
   >
     <!-- NOTE: 考生信息 -->
-    <div h-30 flex="~ column" justify="center">
+    <div flex="~ column" justify="center">
       <a-space flex="~ col">
         <user-outlined />
         <span>用户名称</span>
         <p>剩余时间：120'' 0'</p>
-        <a-button type="danger" class="button-green">
-          提交试卷
-        </a-button>
       </a-space>
     </div>
 
@@ -29,7 +26,14 @@
       <table w-50 h-auto text-center border>
         <tr v-for="item in dataNumber.radio / 5">
           <td v-for="list in 5">
-            {{ (item - 1) * 5 + list }}
+            <a-anchor :affix="false" @click="handleClick">
+              <a-anchor-link
+                :href="
+                  '#question' + ((item - 1) * 5 + list)
+                "
+                :title="(item - 1) * 5 + list"
+              />
+            </a-anchor>
           </td>
         </tr>
       </table>
@@ -37,7 +41,14 @@
       <table w-50 h-auto text-center border>
         <tr v-for="item in dataNumber.checkbox / 5">
           <td v-for="list in 5">
-            {{ (item - 1) * 5 + list }}
+            <a-anchor :affix="false" @click="handleClick">
+              <a-anchor-link
+                :href="
+                  '#checkbox' + ((item - 1) * 5 + list)
+                "
+                :title="(item - 1) * 5 + list"
+              />
+            </a-anchor>
           </td>
         </tr>
       </table>
@@ -45,22 +56,47 @@
       <table w-50 h-auto text-center border>
         <tr v-for="item in dataNumber.judge / 5">
           <td v-for="list in 5">
-            {{ (item - 1) * 5 + list }}
+            <a-anchor :affix="false" @click="handleClick">
+              <a-anchor-link
+                :href="'#judge' + ((item - 1) * 5 + list)"
+                :title="(item - 1) * 5 + list"
+              />
+            </a-anchor>
           </td>
         </tr>
       </table>
+      <div text-center mt-10>
+        <a-button
+          type="danger"
+          class="button-green"
+          w-50
+          rounded
+          @click="finishTask"
+        >
+          提交试卷
+        </a-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { UserOutlined } from '@ant-design/icons-vue'
+import type { AnchorProps } from 'ant-design-vue'
 import { reactive } from 'vue'
+import mitt from '@/utils/mitt'
 const dataNumber = reactive({
   radio: 40,
   checkbox: 30,
   judge: 30,
 })
+const handleClick: AnchorProps['onClick'] = (e, link) => {
+  e.preventDefault()
+  console.log(link)
+}
+const finishTask = () => {
+  mitt.emit('finishTask')
+}
 </script>
 
 <style lang="scss" scoped>
