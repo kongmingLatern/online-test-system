dde1e34d2a181b32d67e6592bdc0c1430716a90e
凤之兮原 <1577310596@qq.com>
2022-12-03 18:22:38 +0800

    enhance: enhanc eModal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,12 +4,13 @@
     data-test="modal"
     :footer="null"
     :width="width"
-    wrap-class-name="full-modal"
   >
     <template #title>
       <span
         data-test="title"
-        :class="isMatch ? 'text-center text-lg font-bold' : null"
+        :class="
+          isMatch ? 'text-center text-lg font-bold' : null
+        "
       >
         {{ title }}
       </span>
@@ -18,37 +19,37 @@
   </a-modal>
 </template>
 <script lang="ts" setup>
-import { inject, type Ref } from "vue";
+import { inject, type Ref } from 'vue'
 
 const props = defineProps<{
-  type?: string;
-  width?: string;
-  isMatch?: boolean;
-}>();
+  type?: string
+  width?: string
+  isMatch?: boolean
+}>()
 
-const title = inject("title") ?? null;
+const title = inject('title') ?? null
 let visible: Ref<boolean | undefined> =
-  props.type === "content"
-    ? inject("isShow")!
-    : props.type === "import"
-    ? inject("impShow")!
-    : inject("comShow")!;
+  props.type === 'content'
+    ? inject('isShow')!
+    : props.type === 'import'
+    ? inject('impShow')!
+    : inject('comShow')!
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss">
 .full-modal {
-  &.ant-modal {
+  .ant-modal {
     max-width: 100%;
     top: 0;
     padding-bottom: 0;
     margin: 0;
   }
-  &.ant-modal-content {
+  .ant-modal-content {
     display: flex;
     flex-direction: column;
     height: calc(100vh);
   }
-  &.ant-modal-body {
+  .ant-modal-body {
     flex: 1;
   }
 }
