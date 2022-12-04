37ed8fb7629f80d723d97576a1cb7b0eb15d8fe9
凤之兮原 <1577310596@qq.com>
2022-12-04 22:29:10 +0800

    remove data

--- src/pages/home/Personal.vue
+++ src/pages/home/Personal.vue
@@ -1,61 +1,15 @@
 <template>
-  <Card :cardList="cardList" data-test="personal" isBase="false"/>
+  <Card
+    :cardList="cardList"
+    data-test="personal"
+    isBase="false"
+  />
 </template>
 
 <script setup lang="ts">
 import { getSelfMatch } from '@/api/request'
 import { onMounted, reactive } from 'vue'
-// const cardList = reactive([
-//   {
-//     matchId: '123',
-//     title: '高等数学A',
-//     taskTime: '2022年9月20日 09:00',
-//     limitTime: '120',
-//   },
-//   {
-//     matchId: '124',
-//     title: '高等数学B',
-//     taskTime: '2022年9月22日 19:00',
-//     limitTime: '20',
-//   },
-//   {
-//     matchId: '125',
-//     title: '高等数学A',
-//     taskTime: '2022年9月20日 09:00',
-//     limitTime: '120',
-//   },
-//   {
-//     matchId: '126',
-//     title: '高等数学B',
-//     taskTime: '2022年9月22日 19:00',
-//     limitTime: '20',
-//   },
-//   {
-//     matchId: '123',
-//     title: '高等数学A',
-//     taskTime: '2022年9月20日 09:00',
-//     limitTime: '120',
-//   },
-//   {
-//     matchId: '123',
-//     title: '高等数学B',
-//     taskTime: '2022年9月22日 19:00',
-//     limitTime: '20',
-//   },
-//   {
-//     matchId: '123',
-//     title: '高等数学A',
-//     taskTime: '2022年9月20日 09:00',
-//     limitTime: '120',
-//   },
-//   {
-//     matchId: '123',
-//     title: '高等数学B',
-//     taskTime: '2022年9月22日 19:00',
-//     limitTime: '20',
-//   },
-// ])
-// const store = useMatch()
+
 const cardList = reactive([])
 onMounted(async () => {
   getSelfMatch(cardList)
