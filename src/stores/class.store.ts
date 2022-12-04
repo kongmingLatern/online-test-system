85cb1b149149d647d6b570471b6dd513c7192dcf
凤之兮原 <1577310596@qq.com>
2022-12-04 14:32:41 +0800

    feat: add class.store.ts

---
+++ src/stores/class.store.ts
@@ -0,0 +1,20 @@
+import http from '@/api/http'
+import { defineStore } from 'pinia'
+
+const getAllClassPath = '/class/getAll'
+export const useClass = defineStore('class', {
+  state: () => ({
+    classList: [] as any[],
+  }),
+  actions: {
+    async getAllClass() {
+      try {
+        const res = await http.get(getAllClassPath)
+        this.classList = res.data
+        return [this.classList, '获取成功']
+      } catch (error) {
+        return [[], '获取失败']
+      }
+    },
+  },
+})
