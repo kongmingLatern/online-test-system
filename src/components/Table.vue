e97f75aabc4df61f85fcd2f8c5967f07e863b716
凤之兮原 <1577310596@qq.com>
2022-12-04 22:28:09 +0800

    format and add grade

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -12,10 +12,16 @@
         <span>{{ index + 1 }}</span>
       </template>
       <template v-if="column.dataIndex === 'status'">
-        <a-tag v-if="record.status === '考完了'" color="red">
+        <a-tag
+          v-if="record.status === '考完了'"
+          color="red"
+        >
           {{ record.status }}
         </a-tag>
-        <a-tag v-if="record.status === '考试中'" color="green">
+        <a-tag
+          v-if="record.status === '考试中'"
+          color="green"
+        >
           {{ record.status }}
         </a-tag>
         <a-tag v-if="record.status === '未开考'">
@@ -31,61 +37,91 @@
           {{ record.status }}
         </a-tag>
       </template>
-      <template v-if="column.dataIndex === 'questionAnswer'">
+      <template
+        v-if="column.dataIndex === 'questionAnswer'"
+      >
         <span>{{ record.questionAnswer }}</span>
       </template>
-      <template v-if="column.dataIndex === 'questionCorrect'">
+      <template
+        v-if="column.dataIndex === 'questionCorrect'"
+      >
         <span>{{ record.questionCorrect }}</span>
       </template>
       <template v-if="column.dataIndex === 'detail'">
         <a>点击查看详情</a>
       </template>
       <template v-if="column.dataIndex === 'delete'">
-        <a-button type="danger" rounded @click="deleteItem(record)">
+        <a-button
+          type="danger"
+          rounded
+          @click="deleteItem(record)"
+        >
           删除
         </a-button>
       </template>
       <template v-if="column.dataIndex === 'deleteAndAuth'">
         <a-space>
           <a-button type="danger" rounded>授权</a-button>
-          <a-button type="danger" rounded @click="deleteItem(record)">
+          <a-button
+            type="danger"
+            rounded
+            @click="deleteItem(record)"
+          >
             删除
           </a-button>
         </a-space>
       </template>
       <template v-if="column.dataIndex === 'student'">
         <a-space>
-          <a-button type="primary" rounded> 重置密码 </a-button>
-          <a-button type="danger" rounded @click="deleteItem(record)">
+          <a-button type="primary" rounded>
+            重置密码
+          </a-button>
+          <a-button
+            type="danger"
+            rounded
+            @click="deleteItem(record)"
+          >
             删除
           </a-button>
         </a-space>
       </template>
+      <templaet v-if="column.dataIndex === 'grade'">
+        <a-tag v-if="record.grade < 60" color="red" text-sm>
+          {{ record.grade }}
+        </a-tag>
+        <a-tag
+          v-else-if="record.grade >= 60"
+          color="green"
+          text-sm
+        >
+          {{ record.grade }}
+        </a-tag>
+      </templaet>
     </template>
   </a-table>
 </template>
 <script lang="ts" setup>
-import { getColumn } from "@/utils";
-import { inject } from "vue";
+import { getColumn } from '@/utils'
+import { inject } from 'vue'
 
-const columnSort = inject("columnSort");
-const data = inject("data");
-const loading = inject("loading");
-const pagination = inject("pagination");
-const change: any = inject("change") ?? null;
-const removeItem: any = inject("removeItem") ?? null;
+const columnSort = inject('columnSort')
+const data = inject('data')
+const loading = inject('loading')
+const pagination = inject('pagination')
+const change: any = inject('change') ?? null
+const removeItem: any = inject('removeItem') ?? null
 // const reset: any = inject('resetItem') ?? null
 
-const columns = getColumn(columnSort);
+const columns = getColumn(columnSort)
 
 // NOTE: 孙传爷
-const changePage = (pagination) => {
-  change(pagination);
-};
+const changePage = pagination => {
+  change(pagination)
+}
 
-const deleteItem = (record) => {
-  removeItem(record);
-};
+const deleteItem = record => {
+  removeItem(record)
+}
 
 // const resetItem = record => reset(record)
 </script>
