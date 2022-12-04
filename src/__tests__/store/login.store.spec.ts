2212ccc186ba07059e012ab7ddff5e0bd0c1abfc
凤之兮原 <1577310596@qq.com>
2022-12-04 15:13:05 +0800

    feat: add login test cases and finish it

---
+++ src/__tests__/store/login.store.spec.ts
@@ -0,0 +1,24 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useLogin } from '@/stores/login.store'
+
+describe('Login Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  // NOTE: 测试接口 '/login' 的数据'
+  it.skip('return success', async () => {
+    const store = useLogin()
+    await expect(store.login('092220120', 'S092220120@'))
+      .resolves.toMatchInlineSnapshot(`
+      [
+        {
+          "id": "1595242006005702658",
+          "studentOrTeacher": 0,
+          "username": "092220120",
+        },
+        "登录成功",
+      ]
+    `)
+  })
+})
