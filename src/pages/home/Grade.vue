d72de234278190c45dffca5877a0c4c62cd60217
凤之兮原 <1577310596@qq.com>
2022-12-04 22:28:43 +0800

    feat: add Grade.vue

--- src/pages/home/Grade.vue
+++ src/pages/home/Grade.vue
@@ -3,9 +3,15 @@
 </template>
 
 <script setup lang="ts">
-import { provide } from 'vue'
-
+import { getSelfGrade } from '@/api/request'
+import { onMounted, provide, reactive } from 'vue'
+const data = reactive([])
+onMounted(async () => {
+  await getSelfGrade(data)
+  console.log(data)
+})
 provide('columnSort', 'queryGrade')
+provide('data', data)
 </script>
 
 <style scoped></style>
