ef10661a87f9042c5db30d3facedf2b87eda6990
凤之兮原 <1577310596@qq.com>
2022-12-04 14:31:28 +0800

    feat: add a param to api

--- src/__tests__/store/task.store.spec.ts
+++ src/__tests__/store/task.store.spec.ts
@@ -1,29 +1,31 @@
-import { useTask } from "@/stores/task.store";
-import { setActivePinia, createPinia } from "pinia";
-import { useBase } from "@/stores/base.store";
-import { resolveSoa } from "dns";
+import { useTask } from '@/stores/task.store'
+import { setActivePinia, createPinia } from 'pinia'
+import { useBase } from '@/stores/base.store'
+import { resolveSoa } from 'dns'
 
-describe("Task Store", () => {
+describe('Task Store', () => {
   beforeEach(() => {
-    setActivePinia(createPinia());
-  });
+    setActivePinia(createPinia())
+  })
 
-  it.skip("shoule return list about task", async () => {
-    const store = useTask();
+  it.skip('shoule return list about task', async () => {
+    const store = useTask()
     await expect(store.getTasksByCurrentPage(1, 1)).resolves
       .toMatchInlineSnapshot(`
       [
         [
           {
             "limitTime": 120,
+            "taskId": "1596427549662015490",
             "taskName": "马原考试",
             "taskPeople": 17,
             "taskStartToEnd": "2022年11月26日 08:00-10:00",
+            "taskTime": undefined,
             "taskType": "期中",
           },
         ],
-        4,
+        7,
       ]
-    `);
-  });
-});
+    `)
+  })
+})
