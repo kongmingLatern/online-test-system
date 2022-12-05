c39bf06984e3a9c91948df51f8500b79647b25c9
凤之兮原 <1577310596@qq.com>
2022-12-05 22:49:58 +0800

    feat: add new Column

--- src/utils/Form/index.ts
+++ src/utils/Form/index.ts
@@ -1,7 +1,8 @@
-import TeacherForm from "./teacher";
-import TaskForm from "./task";
-import StudentForm from "./student";
-import QuestionForm from "./radio";
+import TeacherForm from './teacher'
+import TaskForm from './task'
+import StudentForm from './student'
+import QuestionForm from './radio'
+import ClassForm from './class'
 
 // 表单配置组件 => FormItem.vue
 const formConfig = {
@@ -9,6 +10,7 @@ const formConfig = {
   task: TaskForm,
   student: StudentForm,
   question: QuestionForm,
-};
+  classList: ClassForm,
+}
 
-export { formConfig };
+export { formConfig }
