f5d7d5b26db80948ee23752faa92c84c8bfdcb50
凤之兮原 <1577310596@qq.com>
2022-12-05 16:53:38 +0800

    feat: add disableContextMenu

--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -10,4 +10,32 @@ export const rules = `
 
 四、入场后对号入座，将《准考证》和当次考试规定的其他有效证件放在桌上指定位置以备查验。领到答题卡和试卷后，应在指定位置和规定时间内准确、清楚地填写姓名、准考证号、座位号等试卷识别信息，凡漏填、错填或字迹不清的答卷、答题卡，导致影响评卷结果，责任由考生自负。
 
-五、遇试卷（答题卡）分发错误及试题字迹不清、重印、漏印或缺页等问题，应立即举手报告监考员或请求更换试卷（答题卡）；开考后再行更换的，延误的考试时间不予补偿；涉及试题内容的疑问，不得向监考员询问。`;
+五、遇试卷（答题卡）分发错误及试题字迹不清、重印、漏印或缺页等问题，应立即举手报告监考员或请求更换试卷（答题卡）；开考后再行更换的，延误的考试时间不予补偿；涉及试题内容的疑问，不得向监考员询问。`
+
+// 禁止右侧菜单
+export function disableContextMenu() {
+  // 禁止鼠标选中
+  document.oncontextmenu = new Function(
+    'event.returnValue=false'
+  ) as any
+  document.onselectstart = new Function(
+    'event.returnValue=false'
+  ) as any
+
+  document.addEventListener('keydown', e => {
+    if (e.key === 'F12') {
+      e.preventDefault()
+      e.stopPropagation()
+    } else if (
+      e.ctrlKey &&
+      e.shiftKey &&
+      e.keyCode === 73
+    ) {
+      e.preventDefault()
+      e.stopPropagation()
+    } else if (e.shiftKey && e.keyCode === 121) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+  })
+}
