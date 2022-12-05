b73785b3b85c22f7b3891953f1dd365309f5613c
凤之兮原 <1577310596@qq.com>
2022-12-05 21:21:12 +0800

    enhance: enhance formItem

--- src/components/admin/FormItem.vue
+++ src/components/admin/FormItem.vue
@@ -26,8 +26,15 @@
           : null
       "
     >
-      <Select v-if="item.type === 'select'" @change="getBaseId" />
-      <a-radio-group v-if="item.type === 'radio'" v-model:value="value">
+      <Checkbox v-if="item.type === 'list'" />
+      <Select
+        v-if="item.type === 'select'"
+        @change="getBaseId"
+      />
+      <a-radio-group
+        v-if="item.type === 'radio'"
+        v-model:value="value"
+      >
         <a-radio :value="1">单选题</a-radio>
         <a-radio :value="2">多选题</a-radio>
         <a-radio :value="3">判断题</a-radio>
@@ -67,7 +74,8 @@
     <div v-if="formState[sort].questionCorrectList">
       <p>答案：</p>
       <a-form-item
-        v-for="answer in formState[sort].questionCorrectList"
+        v-for="answer in formState[sort]
+          .questionCorrectList"
         :key="answer.key"
       >
         <a-input
@@ -87,71 +95,79 @@
     </div>
 
     <div text-right>
-      <a-button class="button" html-type="submit" rounded> 提交信息 </a-button>
+      <a-button class="button" html-type="submit" rounded>
+        提交信息
+      </a-button>
     </div>
   </a-form>
 </template>
 <script lang="ts" setup>
-import { inject, reactive, ref, type Ref } from "vue";
-import { getFormItem, getValueByObject, reactiveToCommon } from "@/utils";
-import { PlusOutlined } from "@ant-design/icons-vue";
+import { inject, reactive, ref, type Ref } from 'vue'
+import {
+  getFormItem,
+  getValueByObject,
+  reactiveToCommon,
+} from '@/utils'
+import { PlusOutlined } from '@ant-design/icons-vue'
 
 const props = defineProps<{
-  sort: string;
-}>();
+  sort: string
+}>()
 
-const value = ref<number>(1);
+const value = ref<number>(1)
 
 const layout = {
   // labelCol: { span: 4 },
   wrapperCol: { span: 24 },
-};
+}
 
-const isShow: Ref<boolean | undefined> = inject("isShow")!;
-const finish: (values: any) => void = inject("finish")!;
+const isShow: Ref<boolean | undefined> = inject('isShow')!
+const finish: (values: any) => void = inject('finish')!
 
-const [formArr, form] = getFormItem(props.sort);
-const formState = reactive(form);
+const [formArr, form] = getFormItem(props.sort)
+const formState = reactive(form)
 
 const validateMessages = {
-  required: "${label} is required!",
+  required: '${label} is required!',
   number: {
-    range: "${label} must be between ${min} and ${max}",
+    range: '${label} must be between ${min} and ${max}',
   },
-};
+}
 
 const addDomain = (key: string) => {
-  console.log(formState);
+  console.log(formState)
   formState[props.sort][key].push({
-    values: "",
+    values: '',
     key: Date.now(),
-  });
-};
+  })
+}
 
 const getBaseId = (baseId: string) => {
-  formState[props.sort].baseId = baseId;
-};
+  formState[props.sort].baseId = baseId
+}
 
 const onFinish = (values: any) => {
-  console.log("Success:", values, value.value);
-  isShow.value = false;
+  console.log('Success:', values, value.value)
+  isShow.value = false
 
-  if (props.sort === "question") {
-    formState[props.sort].questionType = value.value;
-    console.log(formState);
-    formState[props.sort].questionCorrectList = getValueByObject(
-      formState[props.sort],
-      "questionCorrectList"
-    );
-    formState[props.sort].questionAnswerList = getValueByObject(
-      formState[props.sort],
-      "questionAnswerList"
-    );
+  if (props.sort === 'question') {
+    formState[props.sort].questionType = value.value
+    console.log(formState)
+    formState[props.sort].questionCorrectList =
+      getValueByObject(
+        formState[props.sort],
+        'questionCorrectList'
+      )
+    formState[props.sort].questionAnswerList =
+      getValueByObject(
+        formState[props.sort],
+        'questionAnswerList'
+      )
   }
   finish({
     ...formState[props.sort],
-  });
-};
+  })
+}
 </script>
 
 <style scoped>
