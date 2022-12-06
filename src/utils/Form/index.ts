337d4bc2f718cabf1bad1586491cc93432be42a5
凤之兮原 <1577310596@qq.com>
2022-12-06 20:00:58 +0800

    enhance: enhance form

--- src/utils/Form/index.ts
+++ src/utils/Form/index.ts
@@ -3,6 +3,7 @@ import TaskForm from './task'
 import StudentForm from './student'
 import QuestionForm from './radio'
 import ClassForm from './class'
+import SubjectForm from './subject'
 
 // 表单配置组件 => FormItem.vue
 const formConfig = {
@@ -11,6 +12,7 @@ const formConfig = {
   student: StudentForm,
   question: QuestionForm,
   classList: ClassForm,
+  list: SubjectForm
 }
 
 export { formConfig }
