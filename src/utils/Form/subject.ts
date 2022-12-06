9b957949dff82d95fa4651c11e9e3c632650314d
凤之兮原 <1577310596@qq.com>
2022-12-06 20:01:17 +0800

    feat: add new form data

---
+++ src/utils/Form/subject.ts
@@ -0,0 +1,12 @@
+export default {
+  subjectName: {
+    name: 'subjectName',
+    type: 'select',
+    label: '科目名称',
+  },
+  baseTitle: {
+    name: 'baseTitle',
+    type: 'text',
+    label: '题库名称',
+  },
+}
