064ed647db1896021999af71112e2734dff1b78d
凤之兮原 <1577310596@qq.com>
2022-12-06 19:59:14 +0800

    enhance: enhance Select.vue

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -9,44 +9,72 @@
   ></a-select>
 </template>
 <script lang="ts" setup>
-import { getBasesByCurrentPage } from "@/api/request";
-import { setReactiveValue } from "@/utils";
-import { onMounted, reactive, ref, watchEffect } from "vue";
+import {
+  getBasesByCurrentPage,
+  getSubjectList,
+} from '@/api/request'
+import { setReactiveValue } from '@/utils'
+import { onMounted, reactive, ref, watchEffect } from 'vue'
 
+const props = defineProps<{
+  type: string
+}>()
 const value = ref<Record<string, any>>({
-  value: "",
-  baseId: "",
-});
-const totalPage = ref<number | undefined>();
-const loading = ref<boolean>(false);
+  value: '',
+  // baseId: '',
+})
+value.value[props.type] = ''
+const totalPage = ref<number | undefined>()
+const loading = ref<boolean>(false)
 
-const selectBaseId = ref<string>("");
+const selectBaseId = ref<string>('')
 
 const emits = defineEmits<{
-  (event: "change", value: string): void;
-}>();
-const options = reactive<any[]>([]);
+  (event: 'change', ...value: any[]): void
+}>()
+const options = reactive<any[]>([])
 onMounted(async () => {
-  const res = await getBasesByCurrentPage(
-    options as any,
-    1,
-    10,
-    totalPage,
-    loading
-  );
-  if (Array.isArray(res)) {
-    value.value = setReactiveValue(res);
-
-    selectBaseId.value = value.value[0].baseId;
+  if (props.type === 'question') {
+    const res = await getBasesByCurrentPage(
+      options as any,
+      1,
+      10,
+      totalPage,
+      loading
+    )
+    if (Array.isArray(res)) {
+      value.value = setReactiveValue(res, 'baseTitle')
+      console.log(value)
+      selectBaseId.value = value.value[0].baseId
+    }
+  } else if (props.type === 'list') {
+    const res = await getSubjectList(options)
+    console.log(options)
+    console.log(res)
+    if (Array.isArray(res)) {
+      value.value = setReactiveValue(res, 'subjectName')
+      console.log(value)
+      selectBaseId.value = value.value[0].subjectId
+    }
   }
-});
+})
 
 watchEffect(() => {
-  emits("change", selectBaseId.value);
-});
+  if (props.type === 'question') {
+    emits('change', 'baseId', selectBaseId.value)
+  } else if (props.type === 'list') {
+    emits('change', 'subjectId', selectBaseId.value)
+  }
+})
 
-const handleChange = (value) => {
-  const { baseId } = value.option;
-  selectBaseId.value = baseId;
-};
+const handleChange = value => {
+  console.log(value)
+  if (props.type === 'question') {
+    const { baseId } = value.option
+    selectBaseId.value = baseId
+  } else if (props.type === 'list') {
+    const { subjectId } = value.option
+    selectBaseId.value = subjectId
+  }
+}
 </script>
