41244d13c90a6541d412ef4fc099a4e833d576b7
凤之兮原 <1577310596@qq.com>
2022-12-08 11:30:47 +0800

    update: update list's data show

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,26 +1,45 @@
 <template>
-  <a-list bordered :data-source="data">
+  <a-list bordered :data-source="data" class="sm">
     <template #renderItem="{ item, index }">
       <a-list-item
         hover="bg-red-800 color-white ease-out duration-300"
       >
-        <div>
+        <a-space>
           <span mr-2 text-sm>{{ index + 1 }}</span>
-          <a-tag color="red" text-sm>{{ item }}</a-tag>
-        </div>
+          <span>
+            考试科目：
+            <a-tag>{{ item.taskName }}</a-tag>
+          </span>
+          <span>
+            考试时间：
+            <a-tag>{{ item.taskStartToEnd }}</a-tag>
+          </span>
+          <span>
+            考试时长：
+            <a-tag>{{ item.limitTime }} 分钟</a-tag>
+          </span>
+        </a-space>
       </a-list-item>
     </template>
   </a-list>
 </template>
 
 <script setup lang="ts">
-const data: string[] = [
-  '高等数学A 2021-06-01 08:00',
-  '高等数学A 2021-06-01 08:00',
-  '高等数学A 2021-06-01 08:00',
-  '高等数学A 2021-06-01 08:00',
-  '高等数学A 2021-06-01 08:00',
-]
+import { getSelfMatch } from '@/api/request'
+import { onMounted, reactive } from 'vue'
+
+const data: any[] = reactive([])
+
+onMounted(async () => {
+  await getSelfMatch(data)
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+@media screen and (max-width: 1000px) {
+  .ant-space {
+    align-items: start;
+    flex-direction: column;
+  }
+}
+</style>
