15823e2fa6b7e2411fa5ef6ce0627a0ce65ee84b
凤之兮原 <1577310596@qq.com>
2022-12-08 09:30:41 +0800

    change: change img

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,7 +1,16 @@
 <template>
-  <a-carousel arrows :after-change="onChange" data-test="carousel" mt-10>
+  <a-carousel
+    arrows
+    :after-change="onChange"
+    data-test="carousel"
+    mt-10
+    autoplay
+  >
     <template #prevArrow>
-      <div class="custom-slick-arrow" style="left: 10px; z-index: 1">
+      <div
+        class="custom-slick-arrow"
+        style="left: 10px; z-index: 1"
+      >
         <left-circle-outlined />
       </div>
     </template>
@@ -12,12 +21,12 @@
     </template>
     <div>
       <img
-        src="https://img2.baidu.com/it/u=505379311,4113824194&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=250"
+        src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp6.itc.cn%2Fq_70%2Fimages03%2F20201217%2F3c44ca2d994e46cc8a6bc2da625e7f08.png&refer=http%3A%2F%2Fp6.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1672993013&t=b46e3410c052bd8df77314c6c7f850d3"
       />
     </div>
     <div>
       <img
-        src="https://img2.baidu.com/it/u=505379311,4113824194&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=250"
+        src="https://img1.baidu.com/it/u=1932080953,1669549261&fm=253&fmt=auto&app=138&f=JPG?w=900&h=383"
       />
     </div>
     <div>
@@ -33,16 +42,61 @@
   </a-carousel>
 </template>
 <script lang="ts" setup>
-import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons-vue";
+import {
+  LeftCircleOutlined,
+  RightCircleOutlined,
+} from '@ant-design/icons-vue'
 const onChange = (current: number) => {
-  console.log(current);
-};
+  console.log(current)
+}
 </script>
 <style scoped>
+@media screen and (max-width: 376px) {
+  .ant-carousel {
+    height: 20vh;
+    line-height: 20vh;
+    overflow: hidden;
+  }
+  .ant-carousel .slick-slide {
+    text-align: center;
+    height: 20vh;
+    line-height: 20vh;
+    background: #364d79;
+    overflow: hidden;
+  }
+  :deep(.ant-carousel .slick-slider) {
+    height: 100%;
+  }
+  div > img {
+    width: 100%;
+    height: 100%;
+  }
+}
+@media screen and (max-width: 1600px) {
+  .ant-carousel {
+    height: 60vh;
+    line-height: 60vh;
+    overflow: hidden;
+  }
+  .ant-carousel .slick-slide {
+    text-align: center;
+    height: 60vh;
+    line-height: 60vh;
+    background: #364d79;
+    overflow: hidden;
+  }
+  :deep(.ant-carousel .slick-slider) {
+    height: 100%;
+  }
+  div > img {
+    width: 100%;
+    height: 60vh;
+  }
+}
 .ant-carousel :deep(.slick-slide) {
   text-align: center;
-  height: 250px;
-  line-height: 250px;
+  height: 53vh;
+  line-height: 53vh;
   background: #364d79;
   overflow: hidden;
 }
@@ -50,11 +104,27 @@ const onChange = (current: number) => {
 .ant-carousel :deep(.slick-slide h3) {
   color: #fff;
 }
+.ant-carousel :deep(.slick-arrow.custom-slick-arrow) {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #fff;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+  z-index: 1;
+}
+.ant-carousel :deep(.custom-slick-arrow:before) {
+  display: none;
+}
+.ant-carousel :deep(.custom-slick-arrow:hover) {
+  opacity: 0.5;
+}
 .ant-carousel {
+  height: 50vh;
   margin-top: 10px;
 }
-img {
+/* img {
   width: 100%;
-  height: 250px;
-}
+  height: 55vh;
+} */
 </style>
