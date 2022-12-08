463d4327b0a1f379720266180c13a01bcae3e303
凤之兮原 <1577310596@qq.com>
2022-12-08 11:31:04 +0800

    change text

--- src/pages/common/Success.vue
+++ src/pages/common/Success.vue
@@ -2,11 +2,11 @@
   <a-result
     status="success"
     title="您已成功交卷"
-    sub-title="请耐心等待，您无需操作，3秒后系统会自动进行跳转"
+    sub-title="请耐心等待，您无需操作，3秒后系统会自动进行跳转至主页"
   >
     <template #extra>
       <a-button key="exit" type="primary" @click="exit">
-        返回登录
+        返回主页
       </a-button>
     </template>
   </a-result>
@@ -18,12 +18,12 @@ import { onMounted } from 'vue'
 
 onMounted(() => {
   setTimeout(() => {
-    router.push('/login')
+    router.push('/home')
   }, 3000)
 })
 
 const exit = () => {
   localStorage.clear()
-  router.push('/login')
+  router.push('/home')
 }
 </script>
