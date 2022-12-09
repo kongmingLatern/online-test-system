b09208385905dba5ebc56d94589bee1fdbc5be2e
凤之兮原 <1577310596@qq.com>
2022-12-09 15:13:00 +0800

    chore: add css

--- src/pages/home/Personal.vue
+++ src/pages/home/Personal.vue
@@ -1,7 +1,7 @@
 <template>
   <div data-test="personal">
     <Card v-if="cardList.length > 0" :cardList="cardList" />
-    <a-empty v-else>
+    <a-empty v-else mt-5>
       <template #description>
         <span>暂无考试</span>
       </template>
