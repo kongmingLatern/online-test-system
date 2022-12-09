273d24fcdcb6031ea4d390cbcc7a606ab8862313
凤之兮原 <1577310596@qq.com>
2022-12-09 14:54:05 +0800

    chore: add build config

--- src/__tests__/components/Search.spec.ts
+++ src/__tests__/components/Search.spec.ts
@@ -1,10 +1,13 @@
-import { findComponent, registGlobalComponent } from "@/utils";
-import Search from "@/components/Search.vue";
+import {
+  findComponent,
+  registGlobalComponent,
+} from '@/utils'
+import Search from '@/components/Search.vue'
 
-const wrapper = registGlobalComponent(Search);
-describe("Search shoule be exists", () => {
-  it("should be exists", () => {
-    const search = findComponent(wrapper, "search");
-    expect(search.exists()).toBe(true);
-  });
-});
+const wrapper = registGlobalComponent(Search as any)
+describe('Search shoule be exists', () => {
+  it('should be exists', () => {
+    const search = findComponent(wrapper, 'search')
+    expect(search.exists()).toBe(true)
+  })
+})
