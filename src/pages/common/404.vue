0bf123e271086e295fe9a2658366b15dd4ac128b
凤之兮原 <1577310596@qq.com>
2022-12-09 14:58:11 +0800

    feat: add 404.vue

---
+++ src/pages/common/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-result status="404" title="404" sub-title="没有该网页">
+    <template #extra>
+      <a-button type="primary" @click="$router.go(-1)">
+        返回上一页
+      </a-button>
+    </template>
+  </a-result>
+</template>
