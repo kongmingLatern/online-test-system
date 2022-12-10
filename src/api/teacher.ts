1bf4a956cfb68a97fe89f4f3af7d629396ca655e
凤之兮原 <1577310596@qq.com>
2022-12-10 22:04:00 +0800

    feat: add finishAddForm.ts

--- src/api/teacher.ts
+++ src/api/teacher.ts
@@ -1,9 +1,20 @@
 import router from '@/router'
+import { useSubject } from '@/stores/subject.store'
 import type { Teacher } from '@/utils'
 import { useTeacher } from '../stores/teacher.store'
 
 export const finishForm = async (values: Teacher) => {
   const store = useTeacher()
   await store.addTeacher(values)
-  router.go(0)
+  // router.go(0)
+}
+
+export const finishAddForm = comShow => async values => {
+  comShow.value = false
+  console.log(values)
+  const store = useSubject()
+  await store.addSubject(values)
+  setTimeout(() => {
+    router.go(0)
+  }, 500)
 }
