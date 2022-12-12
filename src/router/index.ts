afa99bf108a3a542a651c2be5a2cda75265a5e4b
凤之兮原 <1577310596@qq.com>
2022-12-12 10:32:28 +0800

    feat: add meta title

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,9 @@ const router = createRouter({
       path: '/',
       name: 'homeIndex',
       redirect: '/login',
+      meta: {
+        title: '在线考试系统',
+      },
     },
     {
       path: '/home',
@@ -22,6 +25,9 @@ const router = createRouter({
           name: 'index',
           component: () =>
             import('@/pages/home/HomeIndex.vue'),
+          meta: {
+            title: '首页',
+          },
         },
         {
           // 试题库
@@ -29,12 +35,18 @@ const router = createRouter({
           name: 'task',
           component: () =>
             import('@/pages/home/TaskBase.vue'),
+          meta: {
+            title: '试题库',
+          },
         },
         {
           //成绩管理
           path: 'grade',
           name: 'grade',
           component: () => import('@/pages/home/Grade.vue'),
+          meta: {
+            title: '成绩查询',
+          },
         },
         {
           // 我的考试
@@ -42,6 +54,9 @@ const router = createRouter({
           name: 'personal',
           component: () =>
             import('@/pages/home/Personal.vue'),
+          meta: {
+            title: '我的考试',
+          },
         },
       ],
     },
@@ -52,7 +67,8 @@ const router = createRouter({
       component: () => import('@/components/Layout.vue'),
       beforeEnter: (to, from, next) => {
         const role = Number.parseInt(
-          localStorage.getItem('isAuth') as string
+          (localStorage.getItem('identity') as string) ||
+            (localStorage.getItem('isAuth') as string)
         )
         if (role === 1) {
           next()
@@ -67,54 +83,81 @@ const router = createRouter({
           name: 'studentInfoUpload',
           component: () =>
             import('@/pages/admin/StudentInfoUpload.vue'),
+          meta: {
+            title: '学生信息上传',
+          },
         },
         {
           path: 'studentInfoManage',
           name: 'studentInfoManage',
           component: () =>
             import('@/pages/admin/StudentInfoManage.vue'),
+          meta: {
+            title: '学生信息管理',
+          },
         },
         {
           path: 'taskBaseManage',
           name: 'taskBaseManage',
           component: () =>
             import('@/pages/admin/TaskBaseManage.vue'),
+          meta: {
+            title: '试题库管理',
+          },
         },
         {
           path: 'radioManage',
           name: 'radioManage',
           component: () =>
             import('@/pages/admin/RadioManage.vue'),
+          meta: {
+            title: '单选题管理',
+          },
         },
         {
           path: 'checkboxManage',
           name: 'checkboxManage',
           component: () =>
             import('@/pages/admin/CheckBoxManage.vue'),
+          meta: {
+            title: '多选题管理',
+          },
         },
         {
           path: 'judgeManage',
           name: 'judgeManage',
           component: () =>
             import('@/pages/admin/JudgeManage.vue'),
+          meta: {
+            title: '判断题管理',
+          },
         },
         {
           path: 'gradeManage',
           name: 'gradeManage',
           component: () =>
             import('@/pages/admin/GradeManage.vue'),
+          meta: {
+            title: '成绩管理',
+          },
         },
         {
           path: 'taskGenerate',
           name: 'taskGenerate',
           component: () =>
             import('@/pages/admin/TaskGenerate.vue'),
+          meta: {
+            title: '试卷生成',
+          },
         },
         {
           path: 'taskManage',
           name: 'taskManage',
           component: () =>
             import('@/pages/admin/TaskManage.vue'),
+          meta: {
+            title: '试卷管理',
+          },
         },
         {
           path: 'searchStudentPassword',
@@ -123,12 +166,18 @@ const router = createRouter({
             import(
               '@/pages/admin/SearchStudentPassword.vue'
             ),
+          meta: {
+            title: '查询考生密码',
+          },
         },
         {
           path: 'teacherManage',
           name: 'teacherManage',
           component: () =>
             import('@/pages/admin/TeacherManage.vue'),
+          meta: {
+            title: '教师管理',
+          },
         },
       ],
     },
@@ -136,28 +185,40 @@ const router = createRouter({
       path: '/match',
       name: 'match',
       component: () => import('@/pages/home/Match.vue'),
+      meta: {
+        title: '在线考试',
+      },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/pages/Login.vue'),
+      meta: {
+        title: '登录',
+      },
     },
     {
       path: '/success',
       name: 'success',
       component: () => import('@/pages/common/Success.vue'),
+      meta: {
+        title: '提交成功',
+      },
     },
     // 404
     {
       path: '/:pathMatch(.*)*',
       name: 'notFound',
       component: () => import('@/pages/common/404.vue'),
+      meta: {
+        title: '404 NOT FOUND',
+      },
     },
   ],
 })
-
 // 路由守卫
 router.beforeEach((to, from, next) => {
+  document.title = to.meta.title as string
   if (to.path === '/login') {
     localStorage.clear()
     next()
